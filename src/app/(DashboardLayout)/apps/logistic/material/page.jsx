7ec9d85eead4abc1110cc19e78fad1df44dbda6a
Max Oliveira Junior <max.junior>
2024-09-18 00:42:26 -0300

    feat: icons lateral menu

---
+++ src/app/(DashboardLayout)/apps/logistic/material/page.jsx
@@ -0,0 +1,26 @@
+"use client";
+
+// import {KanbanDataContextProvider} from '@/app/context/kanbancontext/index';
+// import Breadcrumb from "@/app/(DashboardLayout)/layout/shared/breadcrumb/Breadcrumb";
+// import PageContainer from "@/app/components/container/PageContainer";
+// import TaskManager from "@/app/components/apps/kanban/TaskManager";
+// import BlankCard from "@/app/components/shared/BlankCard";
+// import { CardContent } from "@mui/material";
+
+const BCrumb = [
+  {
+    to: "/",
+    title: "Home",
+  },
+  {
+    title: "Kanban",
+  },
+];
+
+function page() {
+  return (
+    <div>oi</div>
+  );
+}
+
+export default page;
