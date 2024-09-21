b07f65a21819ef2e534f97d11dcce7f85a239761
Max Oliveira <max-oliveira@outlook.com.br>
2024-09-20 22:35:01 -0300

    feat: Service sale

--- src/services/saleService.js
+++ src/services/saleService.js
@@ -2,12 +2,22 @@ import apiClient from './apiClient';
 
 const saleService = {
     getSales: async () => { 
-        const response = await apiClient.patch(`/api/sales/`);
+        const response = await apiClient.get(`/api/sales/`);
         return response.data;
     },
 
-    updateSale: async (leadId, data) => {
-        const response = await apiClient.patch(`/api/leads/${leadId}/`, data);
+    getSale: async (id) => { 
+        const response = await apiClient.get(`/api/sales/${id}/`);
+        return response.data;
+    },
+
+    updateSale: async (id, data) => {
+        const response = await apiClient.patch(`/api/sales/${id}/`, data);
+        return response.data;
+    },
+
+    createSale: async (data) => {
+        const response = await apiClient.post(`/api/sales/`, data);
         return response.data;
     },
     
