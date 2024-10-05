301a4f36ea52a6334c965e2385ac5146706db476
matheusmacias <matheusmacias3@gmail.com>
2024-10-05 00:13:12 -0300

    feat: add functionality to list projects

---
+++ src/app/(DashboardLayout)/apps/project/page.jsx
@@ -0,0 +1,215 @@
+'use client';
+import React, { useState, useEffect } from "react";
+import {
+    CardContent,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Paper,
+    Typography,
+    IconButton,
+    Tooltip,
+    Chip,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    Button,
+} from '@mui/material';
+import {
+    Edit as EditIcon,
+    Delete as DeleteIcon,
+    CheckCircle as CheckCircleIcon,
+    HourglassEmpty as HourglassEmptyIcon,
+    Cancel as CancelIcon,
+    Add,
+    AddBox,
+    AddBoxTwoTone,
+    AddBoxRounded,
+    Settings,
+} from '@mui/icons-material';
+
+import { useRouter } from 'next/navigation';
+import BlankCard from '@/app/components/shared/BlankCard';
+import PageContainer from "@/app/components/container/PageContainer";
+import projectService from "@/services/projectService";
+
+const getStatusChip = (status) => {
+    switch (status) {
+        case 'P':
+            return <Chip label="Pendente" color="warning" icon={<HourglassEmptyIcon />} />;
+        case 'F':
+            return <Chip label="Finalizado" color="success" icon={<CheckCircleIcon />} />;
+        case 'EA':
+            return <Chip label="Em Andamento" color="primary" icon={<HourglassEmptyIcon />} />;
+        case 'C':
+            return <Chip label="Cancelado" color="error" icon={<CancelIcon />} />;
+        case 'D':
+            return <Chip label="Distrato" color="error" icon={<CancelIcon />} />;
+        default:
+            return <Chip label={status} />;
+    }
+};
+
+const getSupplyTypeChip = (type) => {
+    switch (type) {
+        case 'M':
+            return <Chip label="Monofásico" color="info" icon={<Settings />} />;
+        case 'B':
+            return <Chip label="Bifásico" color="info" icon={<Settings />} />;
+        case 'T':
+            return <Chip label="Trifásico" color="info" icon={<Settings />} />;
+        default:
+            return <Chip label={type} />;
+    }
+};
+
+const SaleList = () => {
+    const [salesList, setSalesList] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [open, setOpen] = useState(false);
+    const [saleToDelete, setSaleToDelete] = useState(null);
+    const router = useRouter(); // Inicializa o roteador
+
+    useEffect(() => {
+        const fetchSales = async () => {
+            try {
+                const data = await projectService.getProjects();
+                console.log(data);
+                setSalesList(data.results);
+            } catch (err) {
+                setError('Erro ao carregar Projetos');
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchSales();
+    }, []);
+
+
+    const handleCreateClick = () => {
+        // router.push('/apps/commercial/sale/create');
+    };
+
+
+    const handleEditClick = (id) => {
+        // router.push(`/apps/commercial/sale/${id}/update`);
+    };
+
+    const handleDeleteClick = (id) => {
+        setSaleToDelete(id);
+        setOpen(true);
+    };
+
+    const handleCloseModal = () => {
+        setOpen(false);
+        setSaleToDelete(null);
+    };
+
+    const handleConfirmDelete = async () => {
+        try {
+            // await saleService.deleteSale(saleToDelete);
+            setSalesList(salesList.filter((item) => item.id !== saleToDelete));
+            console.log('Venda excluída com sucesso');
+        } catch (err) {
+            setError('Erro ao excluir a venda');
+            console.error('Erro ao excluir a venda:', err);
+        } finally {
+            handleCloseModal();
+        }
+    };
+
+    return (
+        <PageContainer title="Projetos" description="Lista de Projetos">
+            <BlankCard>
+                <CardContent>
+                    <Typography variant="h6" gutterBottom>
+                        Lista de Projetos
+                    </Typography>
+                    <Button variant="outlined" startIcon={<AddBoxRounded />} sx={{marginTop:1,marginBottom:2}} onClick={handleCreateClick}>
+                        Criar Projeto
+                    </Button>
+                    {loading ? (
+                        <Typography>Carregando...</Typography>
+                    ) : error ? (
+                        <Typography color="error">{error}</Typography>
+                    ) : (
+                        <TableContainer component={Paper} elevation={3}>
+                            <Table aria-label="sales table">
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>ID</TableCell>
+                                        <TableCell>Nome contratante</TableCell>
+                                        <TableCell>Número do Contrato</TableCell>
+                                        <TableCell>Status do Projeto</TableCell>
+                                        <TableCell>Tipo de Forneciemento</TableCell>
+                                        <TableCell>KWp</TableCell>
+                                        <TableCell>Ações</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {salesList.map((item) => (
+                                        <TableRow key={item.id} hover>
+                                            <TableCell>{item.id}</TableCell>
+                                            <TableCell>{item.sale?.customer?.complete_name}</TableCell>
+                                            <TableCell>{item.sale?.contract_number}</TableCell>
+                                            <TableCell>{getStatusChip(item.status)}</TableCell>
+                                            <TableCell>{getSupplyTypeChip(item.supply_type)}</TableCell>
+                                            <TableCell>{item.kwp}</TableCell>
+                                            <TableCell>
+                                                <Tooltip title="Editar">
+                                                    <IconButton 
+                                                        color="primary" 
+                                                        size="small" 
+                                                        onClick={() => handleEditClick(item.id)}
+                                                    >
+                                                        <EditIcon />
+                                                    </IconButton>
+                                                </Tooltip>
+                                                <Tooltip title="Excluir">
+                                                    <IconButton 
+                                                        color="error" 
+                                                        size="small" 
+                                                        onClick={() => handleDeleteClick(item.id)}
+                                                    >
+                                                        <DeleteIcon />
+                                                    </IconButton>
+                                                </Tooltip>
+                                            </TableCell>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    )}
+                </CardContent>
+            </BlankCard>
+
+            {/* Modal de confirmação de exclusão */}
+            <Dialog open={open} onClose={handleCloseModal}>
+                <DialogTitle>Confirmar Exclusão</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Tem certeza de que deseja excluir esta venda? Esta ação não pode ser desfeita.
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseModal} color="primary">
+                        Cancelar
+                    </Button>
+                    <Button onClick={handleConfirmDelete} color="error">
+                        Excluir
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </PageContainer>
+    );
+};
+
+export default SaleList;
---
+++ src/services/projectService.js
@@ -0,0 +1,31 @@
+import apiClient from './apiClient';
+
+const projectService = {
+    getProjects: async () => { 
+        const response = await apiClient.get(`/api/projects/`);
+        return response.data;
+    },
+
+    getProjectById: async (id) => { 
+        const response = await apiClient.get(`/api/projects/${id}/`);
+        return response.data;
+    },
+
+    updateProject: async (id, data) => {
+        const response = await apiClient.put(`/api/projects/${id}/`, data);
+        return response.data;
+    },
+
+    createProject: async (data) => {
+        const response = await apiClient.post(`/api/projects/`, data);
+        return response.data;
+    },
+
+    deleteProject: async (id) => {
+        const response = await apiClient.delete(`/api/projects/${id}/`);
+        return response.data;
+    },
+    
+};
+
+export default projectService;
