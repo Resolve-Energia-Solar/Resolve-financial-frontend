768d594419fbc44b51521746c98e791c6d7951fc
matheusmacias <matheusmacias3@gmail.com>
2024-10-06 19:39:24 -0300

    refactor: update layout of marketing campaign list

--- src/app/(DashboardLayout)/apps/campaign/page.jsx
+++ src/app/(DashboardLayout)/apps/campaign/page.jsx
@@ -1,242 +1,189 @@
 'use client';
+import React, { useState, useEffect } from "react";
+import {
+    CardContent,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Paper,
+    Typography,
+    IconButton,
+    Tooltip,
+    Chip,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    Button,
+} from '@mui/material';
+import {
+    Edit as EditIcon,
+    Delete as DeleteIcon,
+    CheckCircle as CheckCircleIcon,
+    HourglassEmpty as HourglassEmptyIcon,
+    Cancel as CancelIcon,
+    AddBoxRounded,
+} from '@mui/icons-material';
 
-import React, { useState, useEffect } from 'react';
-import Box from '@mui/material/Box';
-import IconButton from '@mui/material/IconButton';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Tooltip from '@mui/material/Tooltip';
-import Typography from '@mui/material/Typography';
-import { IconTrash, IconHash, IconMapPin, IconUsers, IconEdit } from '@tabler/icons-react';
-import { Slide, Zoom, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Button } from '@mui/material';
-import Breadcrumb from "@/app/(DashboardLayout)/layout/shared/breadcrumb/Breadcrumb";
-import PageContainer from "@/app/components/container/PageContainer";
-import ChildCard from '@/app/components/shared/ChildCard';
-import campaignService from '@/services/campaignService';
 import { useRouter } from 'next/navigation';
+import BlankCard from '@/app/components/shared/BlankCard';
+import PageContainer from "@/app/components/container/PageContainer";
+import campaignService from "@/services/campaignService"; // Altere para o serviço de campanha
 
-const BCrumb = [
-  {
-    to: "/",
-    title: "Home",
-  },
-  {
-    title: "Campanhas",
-  },
-];
+const getStatusChip = (status) => {
+    switch (status) {
+        case 'F':
+            return <Chip label="Finalizado" color="success" icon={<CheckCircleIcon />} />;
+        case 'EA':
+            return <Chip label="Em Andamento" color="primary" icon={<HourglassEmptyIcon />} />;
+        case 'C':
+            return <Chip label="Cancelado" color="error" icon={<CancelIcon />} />;
+        case 'D':
+            return <Chip label="Distrato" color="error" icon={<CancelIcon />} />;
+        default:
+            return <Chip label={status} />;
+    }
+};
 
-function Page() {
-  const [campaign, setCampaign] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [open, setOpen] = useState(false);
-  const [campaignToDelete, setCampaignToDelete] = useState(null);
+const CampaignList = () => {
+    const [campaignsList, setCampaignsList] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [open, setOpen] = useState(false);
+    const [campaignToDelete, setCampaignToDelete] = useState(null);
+    const router = useRouter();
 
-  const router = useRouter();
+    useEffect(() => {
+        const fetchCampaigns = async () => {
+            try {
+                const data = await campaignService.getCampaigns();
+                setCampaignsList(data.results);
+            } catch (err) {
+                setError('Erro ao carregar campanhas');
+            } finally {
+                setLoading(false);
+            }
+        };
 
-  useEffect(() => {
-    const fetchCampaign = async () => {
-      try {
-        const data = await campaignService.getCampaigns();
-        setCampaign(data.results);
-      } catch (err) {
-        setError('Erro ao carregar campanhas');
-      } finally {
-        setLoading(false);
-      }
+        fetchCampaigns();
+    }, []);
+
+    const handleCreateClick = () => {
+        router.push('/apps/campaign/create'); // Alterado para a rota de criação de campanha
     };
 
-    fetchCampaign();
-  }, []);
+    const handleEditClick = (id) => {
+        router.push(`/apps/campaign/${id}/update`); // Alterado para a rota de edição de campanha
+    };
 
-  if (loading) {
-    return <div>Carregando...</div>;
-  }
+    const handleDeleteClick = (id) => {
+        setCampaignToDelete(id);
+        setOpen(true);
+    };
 
-  if (error) {
-    return <div>{error}</div>;
-  }
+    const handleCloseModal = () => {
+        setOpen(false);
+        setCampaignToDelete(null);
+    };
 
-  const handleEditClick = (id) => {
-      router.push(`/apps/campaign/${id}/update`);
-  };
+    const handleConfirmDelete = async () => {
+        try {
+            await campaignService.deleteCampaign(campaignToDelete); // Chamada ao serviço de exclusão de campanha
+            setCampaignsList(campaignsList.filter((item) => item.id !== campaignToDelete));
+            console.log('Campanha excluída com sucesso');
+        } catch (err) {
+            setError('Erro ao excluir a campanha');
+            console.error('Erro ao excluir a campanha:', err);
+        } finally {
+            handleCloseModal();
+        }
+    };
 
-  const handleDeleteClick = (id) => {
-    setCampaignToDelete(id);
-    setOpen(true);
-  };
+    return (
+        <PageContainer title="Campanhas" description="Lista de Campanhas">
+            <BlankCard>
+                <CardContent>
+                    <Typography variant="h6" gutterBottom>
+                        Lista de Campanhas
+                    </Typography>
+                    <Button variant="outlined" startIcon={<AddBoxRounded />} sx={{ marginTop: 1, marginBottom: 2 }} onClick={handleCreateClick}>
+                        Adicionar Campanha
+                    </Button>
+                    {loading ? (
+                        <Typography>Carregando...</Typography>
+                    ) : error ? (
+                        <Typography color="error">{error}</Typography>
+                    ) : (
+                        <TableContainer component={Paper} elevation={3}>
+                            <Table aria-label="campaigns table">
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>ID</TableCell>
+                                        <TableCell>Nome da Campanha</TableCell>
+                                        <TableCell>Data de Início</TableCell>
+                                        <TableCell>Data de Término</TableCell>
+                                        <TableCell>Ações</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {campaignsList.map((item) => (
+                                        <TableRow key={item.id} hover>
+                                            <TableCell>{item.id}</TableCell>
+                                            <TableCell>{item.name}</TableCell>
+                                            <TableCell>{new Date(item.start_datetime).toLocaleDateString()}</TableCell>
+                                            <TableCell>{new Date(item.end_datetime).toLocaleDateString()}</TableCell>
+                                            <TableCell>
+                                                <Tooltip title="Editar">
+                                                    <IconButton 
+                                                        color="primary" 
+                                                        size="small" 
+                                                        onClick={() => handleEditClick(item.id)}
+                                                    >
+                                                        <EditIcon />
+                                                    </IconButton>
+                                                </Tooltip>
+                                                <Tooltip title="Excluir">
+                                                    <IconButton 
+                                                        color="error" 
+                                                        size="small" 
+                                                        onClick={() => handleDeleteClick(item.id)}
+                                                    >
+                                                        <DeleteIcon />
+                                                    </IconButton>
+                                                </Tooltip>
+                                            </TableCell>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    )}
+                </CardContent>
+            </BlankCard>
 
-  const handleCloseModal = () => {
-    setOpen(false);
-    setCampaignToDelete(null);
-  };
+            {/* Modal de confirmação de exclusão */}
+            <Dialog open={open} onClose={handleCloseModal}>
+                <DialogTitle>Confirmar Exclusão</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Tem certeza de que deseja excluir esta campanha? Esta ação não pode ser desfeita.
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseModal} color="primary">
+                        Cancelar
+                    </Button>
+                    <Button onClick={handleConfirmDelete} color="error">
+                        Excluir
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </PageContainer>
+    );
+};
 
-  const handleConfirmDelete = async () => {
-    try {
-      // Uncomment the following line when you have the delete function ready
-      // await campaignService.deleteCampaign(campaignToDelete);
-      setCampaign(campaign.filter((item) => item.id !== campaignToDelete));
-      console.log('Campanha excluída com sucesso');
-    } catch (err) {
-      setError('Erro ao excluir a campanha');
-      console.error('Erro ao excluir a campanha:', err);
-    } finally {
-      handleCloseModal();
-    }
-  };
-
-  return (
-    <PageContainer title="Filiais" description="Lista de filiais">
-      <Breadcrumb title="Filiais" items={BCrumb} />
-      <ChildCard>
-        {campaign.length > 0 ? (
-          <Box mt={4}>
-            <Slide direction="up" in={true} mountOnEnter unmountOnExit>
-              <TableContainer
-                sx={{
-                  boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-                  borderRadius: '8px',
-                  overflow: 'hidden',
-                  backgroundColor: '#f9f9f9',
-                }}
-              >
-                <Table>
-                  <TableHead>
-                    <TableRow>
-                      <TableCell>
-                        <Typography variant="subtitle1" sx={{ display: 'flex', alignItems: 'center' }}>
-                          <IconHash size={20} style={{ marginRight: '8px' }} />
-                          Id
-                        </Typography>
-                      </TableCell>
-                      <TableCell>
-                        <Typography variant="subtitle1" sx={{ display: 'flex', alignItems: 'center' }}>
-                          <IconUsers size={20} style={{ marginRight: '8px' }} />
-                          Nome
-                        </Typography>
-                      </TableCell>
-                      <TableCell>
-                        <Typography variant="subtitle1" sx={{ display: 'flex', alignItems: 'center' }}>
-                          <IconMapPin size={20} style={{ marginRight: '8px' }} />
-                          Descrição
-                        </Typography>
-                      </TableCell>
-                      <TableCell>
-                        <Typography variant="subtitle1" sx={{ display: 'flex', alignItems: 'center' }}>
-                          <IconUsers size={20} style={{ marginRight: '8px' }} />
-                          Data inicial
-                        </Typography>
-                      </TableCell>
-                      <TableCell>
-                        <Typography variant="subtitle1" sx={{ display: 'flex', alignItems: 'center' }}>
-                          <IconUsers size={20} style={{ marginRight: '8px' }} />
-                          Data final
-                        </Typography>
-                      </TableCell>
-                      <TableCell align="right">
-                        <Typography variant="subtitle1">Ação</Typography>
-                      </TableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {campaign.map((campaignItem, index) => (
-                      <Zoom key={campaignItem.id} in={true} style={{ transitionDelay: `${index * 100}ms` }}>
-                        <TableRow hover>
-                          <TableCell>
-                            <Typography variant="body1" color="textSecondary">
-                              {campaignItem.id}
-                            </Typography>
-                          </TableCell>
-
-                          <TableCell>
-                            <Typography variant="subtitle1" fontWeight={600} noWrap>
-                              {campaignItem.name}
-                            </Typography>
-                          </TableCell>
-                          <TableCell>
-                            <Typography variant="subtitle1" fontWeight={600} noWrap>
-                              {campaignItem.description}
-                            </Typography>
-                          </TableCell>
-
-                          <TableCell>
-                            <Typography variant="subtitle1" fontWeight={600} noWrap>
-                              {new Date(campaignItem.start_datetime).toLocaleDateString()}
-                            </Typography>
-                          </TableCell>
-                          <TableCell>
-                            <Typography variant="subtitle1" fontWeight={600} noWrap>
-                              {new Date(campaignItem.end_datetime).toLocaleDateString()}
-                            </Typography>
-                          </TableCell>
-
-                          <TableCell align="right">
-                            <Tooltip title="Editar Filial">
-                              <IconButton
-                                onClick={() => handleEditClick(campaignItem.id)}
-                                sx={{
-                                  transition: 'all 0.3s ease',
-                                  '&:hover': {
-                                    backgroundColor: 'rgba(0, 0, 255, 0.1)',
-                                    transform: 'scale(1.1)',
-                                  },
-                                }}
-                              >
-                                <IconEdit size="24" />
-                              </IconButton>
-                            </Tooltip>
-
-                            <Tooltip title="Deletar Filial">
-                              <IconButton
-                                onClick={() => handleDeleteClick(campaignItem.id)}
-                                sx={{
-                                  transition: 'all 0.3s ease',
-                                  '&:hover': {
-                                    backgroundColor: 'rgba(255, 0, 0, 0.1)',
-                                    transform: 'scale(1.1)',
-                                  },
-                                }}
-                              >
-                                <IconTrash size="24" />
-                              </IconButton>
-                            </Tooltip>
-                          </TableCell>
-                        </TableRow>
-                      </Zoom>
-                    ))}
-                  </TableBody>
-                </Table>
-              </TableContainer>
-            </Slide>
-          </Box>
-        ) : (
-          <Typography variant="body1" color="textSecondary" align="center">Nenhuma campanha encontrada.</Typography>
-        )}
-      </ChildCard>
-
-      {/* Modal de confirmação de exclusão */}
-      <Dialog open={open} onClose={handleCloseModal}>
-        <DialogTitle>Confirmar Exclusão</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Tem certeza de que deseja excluir esta campanha? Esta ação não pode ser desfeita.
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleCloseModal} color="primary">
-            Cancelar
-          </Button>
-          <Button onClick={handleConfirmDelete} color="error">
-            Excluir
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </PageContainer>
-  );
-}
-
-export default Page;
+export default CampaignList;
