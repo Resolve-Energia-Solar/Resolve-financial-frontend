a65b761d76173aec0db4efc950731197e3906af5
matheusmacias <matheusmacias3@gmail.com>
2024-10-06 19:11:32 -0300

    feat: implement functionality to update marketing campaigns

--- src/app/(DashboardLayout)/apps/campaign/page.jsx
+++ src/app/(DashboardLayout)/apps/campaign/page.jsx
@@ -1,11 +1,24 @@
 'use client';
 
 import React, { useState, useEffect } from 'react';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Tooltip from '@mui/material/Tooltip';
+import Typography from '@mui/material/Typography';
+import { IconTrash, IconHash, IconMapPin, IconUsers, IconEdit } from '@tabler/icons-react';
+import { Slide, Zoom } from '@mui/material';
 import Breadcrumb from "@/app/(DashboardLayout)/layout/shared/breadcrumb/Breadcrumb";
 import PageContainer from "@/app/components/container/PageContainer";
 import ChildCard from '@/app/components/shared/ChildCard';
 import campaignService from '@/services/campaignService';
-import ListItem from '@/app/components/apps/campaign/ListItem';
+import { useRouter } from 'next/navigation';
+
 
 const BCrumb = [
   {
@@ -22,12 +35,14 @@ function Page() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
+  const router = useRouter();
+
   useEffect(() => {
     const fetchCampaign = async () => {
       try {
-        const data = await campaignService.getCampaign();
+        const data = await campaignService.getCampaigns();
         setLoading(false);
-        console.log(data)
+        console.log(data);
         setCampaign(data.results);
       } catch (err) {
         setError('Erro ao carregar campaign');
@@ -47,12 +62,134 @@ function Page() {
     return <div>{error}</div>;
   }
 
+  const handleEditClick = (id) => {
+      router.push(`/apps/campaign/${id}/update`);
+  };
+
   return (
     <PageContainer title="Filiais" description="Lista de filiais">
       <Breadcrumb title="Filiais" items={BCrumb} />
       <ChildCard>
         {campaign.length > 0 ? (
-          <ListItem campaigns={campaign} onDelete={(id) => console.log(`Deletar filial ${id}`)} />
+          <Box mt={4}>
+            <Slide direction="up" in={true} mountOnEnter unmountOnExit>
+              <TableContainer
+                sx={{
+                  boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+                  borderRadius: '8px',
+                  overflow: 'hidden',
+                  backgroundColor: '#f9f9f9',
+                }}
+              >
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>
+                        <Typography variant="subtitle1" sx={{ display: 'flex', alignItems: 'center' }}>
+                          <IconHash size={20} style={{ marginRight: '8px' }} />
+                          Id
+                        </Typography>
+                      </TableCell>
+                      <TableCell>
+                        <Typography variant="subtitle1" sx={{ display: 'flex', alignItems: 'center' }}>
+                          <IconUsers size={20} style={{ marginRight: '8px' }} />
+                          Nome
+                        </Typography>
+                      </TableCell>
+                      <TableCell>
+                        <Typography variant="subtitle1" sx={{ display: 'flex', alignItems: 'center' }}>
+                          <IconMapPin size={20} style={{ marginRight: '8px' }} />
+                          Descrição
+                        </Typography>
+                      </TableCell>
+                      <TableCell>
+                        <Typography variant="subtitle1" sx={{ display: 'flex', alignItems: 'center' }}>
+                          <IconUsers size={20} style={{ marginRight: '8px' }} />
+                          Data inicial
+                        </Typography>
+                      </TableCell>
+                      <TableCell>
+                        <Typography variant="subtitle1" sx={{ display: 'flex', alignItems: 'center' }}>
+                          <IconUsers size={20} style={{ marginRight: '8px' }} />
+                          Data final
+                        </Typography>
+                      </TableCell>
+                      <TableCell align="right">
+                        <Typography variant="subtitle1">Ação</Typography>
+                      </TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {campaign.map((campaignItem, index) => (
+                      <Zoom key={campaignItem.id} in={true} style={{ transitionDelay: `${index * 100}ms` }}>
+                        <TableRow hover>
+                          <TableCell>
+                            <Typography variant="body1" color="textSecondary">
+                              {campaignItem.id}
+                            </Typography>
+                          </TableCell>
+
+                          <TableCell>
+                            <Typography variant="subtitle1" fontWeight={600} noWrap>
+                              {campaignItem.name}
+                            </Typography>
+                          </TableCell>
+                          <TableCell>
+                            <Typography variant="subtitle1" fontWeight={600} noWrap>
+                              {campaignItem.description}
+                            </Typography>
+                          </TableCell>
+
+                          <TableCell>
+                            <Typography variant="subtitle1" fontWeight={600} noWrap>
+                              {new Date(campaignItem.start_datetime).toLocaleDateString()}
+                            </Typography>
+                          </TableCell>
+                          <TableCell>
+                            <Typography variant="subtitle1" fontWeight={600} noWrap>
+                              {new Date(campaignItem.end_datetime).toLocaleDateString()}
+                            </Typography>
+                          </TableCell>
+
+                          <TableCell align="right">
+                            <Tooltip title="Editar Filial">
+                              <IconButton
+                                onClick={() => handleEditClick(campaignItem.id)}
+                                sx={{
+                                  transition: 'all 0.3s ease',
+                                  '&:hover': {
+                                    backgroundColor: 'rgba(0, 0, 255, 0.1)',
+                                    transform: 'scale(1.1)',
+                                  },
+                                }}
+                              >
+                                <IconEdit size="24" />
+                              </IconButton>
+                            </Tooltip>
+
+                            <Tooltip title="Deletar Filial">
+                              <IconButton
+                                onClick={() => console.log(`Deletar filial ${campaignItem.id}`)}
+                                sx={{
+                                  transition: 'all 0.3s ease',
+                                  '&:hover': {
+                                    backgroundColor: 'rgba(255, 0, 0, 0.1)',
+                                    transform: 'scale(1.1)',
+                                  },
+                                }}
+                              >
+                                <IconTrash size="24" />
+                              </IconButton>
+                            </Tooltip>
+                          </TableCell>
+                        </TableRow>
+                      </Zoom>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </Slide>
+          </Box>
         ) : (
           <div>Nenhuma filial encontrada</div>
         )}
--- src/app/components/apps/campaign/ListItem.jsx
+++ src/app/components/apps/campaign/ListItem.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
 import Table from '@mui/material/Table';
@@ -11,121 +11,180 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Tooltip from '@mui/material/Tooltip';
 import Typography from '@mui/material/Typography';
-import { IconTrash, IconHash, IconMapPin, IconUsers } from '@tabler/icons-react';
-import { Slide, Switch, Zoom } from '@mui/material';
+import { IconTrash, IconHash, IconMapPin, IconUsers, IconEdit } from '@tabler/icons-react';
+import { Slide, Zoom } from '@mui/material';
+import Breadcrumb from "@/app/(DashboardLayout)/layout/shared/breadcrumb/Breadcrumb";
+import PageContainer from "@/app/components/container/PageContainer";
+import ChildCard from '@/app/components/shared/ChildCard';
+import campaignService from '@/services/campaignService';
 
-const ListItem = ({ campaigns = [], onDelete }) => {
-  if (!campaigns || campaigns.length === 0) {
-    return <div>Nenhuma filial encontrada.</div>;
+const BCrumb = [
+  {
+    to: "/",
+    title: "Home",
+  },
+  {
+    title: "Filiais",
+  },
+];
+
+function Page() {
+  const [campaign, setCampaign] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchCampaign = async () => {
+      try {
+        const data = await campaignService.getCampaigns();
+        setLoading(false);
+        console.log(data);
+        setCampaign(data.results);
+      } catch (err) {
+        setError('Erro ao carregar campaign');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCampaign();
+  }, []);
+
+  if (loading) {
+    return <div>Carregando...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
   }
 
   return (
-    <Box mt={4}>
-      <Slide direction="up" in={true} mountOnEnter unmountOnExit>
-        <TableContainer
-          sx={{
-            boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-            borderRadius: '8px',
-            overflow: 'hidden',
-            backgroundColor: '#f9f9f9',
-          }}
-        >
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>
-                  <Typography variant="subtitle1" sx={{ display: 'flex', alignItems: 'center' }}>
-                    <IconHash size={20} style={{ marginRight: '8px' }} />
-                    Id
-                  </Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography variant="subtitle1" sx={{ display: 'flex', alignItems: 'center' }}>
-                    <IconUsers size={20} style={{ marginRight: '8px' }} />
-                    Nome
-                  </Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography variant="subtitle1" sx={{ display: 'flex', alignItems: 'center' }}>
-                    <IconMapPin size={20} style={{ marginRight: '8px' }} />
-                    Descrição
-                  </Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography variant="subtitle1" sx={{ display: 'flex', alignItems: 'center' }}>
-                    <IconUsers size={20} style={{ marginRight: '8px' }} />
-                    Data inicial
-                  </Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography variant="subtitle1" sx={{ display: 'flex', alignItems: 'center' }}>
-                    <IconUsers size={20} style={{ marginRight: '8px' }} />
-                    Data final
-                  </Typography>
-                </TableCell>
-                <TableCell align="right">
-                  <Typography variant="subtitle1">Ação</Typography>
-                </TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {campaigns.map((campaign, index) => (
-                <Zoom key={campaign.id} in={true} style={{ transitionDelay: `${index * 100}ms` }}>
-                  <TableRow hover>
-                    <TableCell>
-                      <Typography variant="body1" color="textSecondary">
-                        {campaign.id}
-                      </Typography>
-                    </TableCell>
+    <PageContainer title="Filiais" description="Lista de filiais">
+        {campaign.length > 0 ? (
+          <Box mt={4}>
+            <Slide direction="up" in={true} mountOnEnter unmountOnExit>
+              <TableContainer
+                sx={{
+                  boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+                  borderRadius: '8px',
+                  overflow: 'hidden',
+                  backgroundColor: '#f9f9f9',
+                }}
+              >
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>
+                        <Typography variant="subtitle1" sx={{ display: 'flex', alignItems: 'center' }}>
+                          <IconHash size={20} style={{ marginRight: '8px' }} />
+                          Id
+                        </Typography>
+                      </TableCell>
+                      <TableCell>
+                        <Typography variant="subtitle1" sx={{ display: 'flex', alignItems: 'center' }}>
+                          <IconUsers size={20} style={{ marginRight: '8px' }} />
+                          Nome
+                        </Typography>
+                      </TableCell>
+                      <TableCell>
+                        <Typography variant="subtitle1" sx={{ display: 'flex', alignItems: 'center' }}>
+                          <IconMapPin size={20} style={{ marginRight: '8px' }} />
+                          Descrição
+                        </Typography>
+                      </TableCell>
+                      <TableCell>
+                        <Typography variant="subtitle1" sx={{ display: 'flex', alignItems: 'center' }}>
+                          <IconUsers size={20} style={{ marginRight: '8px' }} />
+                          Data inicial
+                        </Typography>
+                      </TableCell>
+                      <TableCell>
+                        <Typography variant="subtitle1" sx={{ display: 'flex', alignItems: 'center' }}>
+                          <IconUsers size={20} style={{ marginRight: '8px' }} />
+                          Data final
+                        </Typography>
+                      </TableCell>
+                      <TableCell align="right">
+                        <Typography variant="subtitle1">Ação</Typography>
+                      </TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {campaign.map((campaignItem, index) => (
+                      <Zoom key={campaignItem.id} in={true} style={{ transitionDelay: `${index * 100}ms` }}>
+                        <TableRow hover>
+                          <TableCell>
+                            <Typography variant="body1" color="textSecondary">
+                              {campaignItem.id}
+                            </Typography>
+                          </TableCell>
 
-                    <TableCell>
-                      <Typography variant="subtitle1" fontWeight={600} noWrap>
-                        {campaign.name}
-                      </Typography>
-                    </TableCell>
-                    <TableCell>
-                      <Typography variant="subtitle1" fontWeight={600} noWrap>
-                        {campaign.description}
-                      </Typography>
-                    </TableCell>
+                          <TableCell>
+                            <Typography variant="subtitle1" fontWeight={600} noWrap>
+                              {campaignItem.name}
+                            </Typography>
+                          </TableCell>
+                          <TableCell>
+                            <Typography variant="subtitle1" fontWeight={600} noWrap>
+                              {campaignItem.description}
+                            </Typography>
+                          </TableCell>
 
-                    <TableCell>
-                      <Typography variant="subtitle1" fontWeight={600} noWrap>
-         
-                        {new Date(campaign.start_datetime).toLocaleDateString()}
-                      </Typography>
-                    </TableCell>
-                    <TableCell>
-                      <Typography variant="subtitle1" fontWeight={600} noWrap>
-                      {new Date(campaign.end_datetime).toLocaleDateString()}
-                      </Typography>
-                    </TableCell>
+                          <TableCell>
+                            <Typography variant="subtitle1" fontWeight={600} noWrap>
+                              {new Date(campaignItem.start_datetime).toLocaleDateString()}
+                            </Typography>
+                          </TableCell>
+                          <TableCell>
+                            <Typography variant="subtitle1" fontWeight={600} noWrap>
+                              {new Date(campaignItem.end_datetime).toLocaleDateString()}
+                            </Typography>
+                          </TableCell>
 
-                    <TableCell align="right">
-                      <Tooltip title="Deletar Filial">
-                        <Switch
-                          onClick={() => onDelete(campaign.id)}
-                          sx={{
-                            transition: 'all 0.3s ease',
-                            '&:hover': {
-                              backgroundColor: 'rgba(255, 0, 0, 0.1)',
-                              transform: 'scale(1.1)',
-                            },
-                          }}
-                        >
-                          <IconTrash size="24" />
-                        </Switch>
-                      </Tooltip>
-                    </TableCell>
-                  </TableRow>
-                </Zoom>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Slide>
-    </Box>
+                          <TableCell align="right">
+                            <Tooltip title="Editar Filial">
+                              <IconButton
+                                onClick={() => console.log(`Editar filial ${campaignItem.id}`)}
+                                sx={{
+                                  transition: 'all 0.3s ease',
+                                  '&:hover': {
+                                    backgroundColor: 'rgba(0, 0, 255, 0.1)',
+                                    transform: 'scale(1.1)',
+                                  },
+                                }}
+                              >
+                                <IconEdit size="24" />
+                              </IconButton>
+                            </Tooltip>
+
+                            <Tooltip title="Deletar Filial">
+                              <IconButton
+                                onClick={() => console.log(`Deletar filial ${campaignItem.id}`)}
+                                sx={{
+                                  transition: 'all 0.3s ease',
+                                  '&:hover': {
+                                    backgroundColor: 'rgba(255, 0, 0, 0.1)',
+                                    transform: 'scale(1.1)',
+                                  },
+                                }}
+                              >
+                                <IconTrash size="24" />
+                              </IconButton>
+                            </Tooltip>
+                          </TableCell>
+                        </TableRow>
+                      </Zoom>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </Slide>
+          </Box>
+        ) : (
+          <div>Nenhuma filial encontrada</div>
+        )}
+    </PageContainer>
   );
-};
+}
 
-export default ListItem;
+export default Page;
