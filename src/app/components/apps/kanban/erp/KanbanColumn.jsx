3ee043e899c43a581471adb381bf5bdecb1dd160
Max Oliveira <max-oliveira@outlook.com.br>
2024-10-30 00:57:19 -0300

    feat: kanban task

--- src/app/components/apps/kanban/erp/KanbanColumn.jsx
+++ src/app/components/apps/kanban/erp/KanbanColumn.jsx
@@ -4,11 +4,14 @@ import KanbanCard from './KanbanCard';
 
 export default function KanbanColumn({ title, tasks }) {
   return (
-    <Box sx={{ width: '300px', mx: 1, p: 2, bgcolor: '#f4f6f8', borderRadius: '8px' }}>
-      <Typography variant="h5" sx={{ mb: 2 }}>{title}</Typography>
-      {tasks.map((task) => (
-        <KanbanCard key={task.id} task={task} />
-      ))}
-    </Box>
+    <Box sx={{ maxWidth: '300px', height: '80vh', minWidth: '300px', mx: 1,py: 3, bgcolor: '#f4f6f8', borderRadius: '8px' }}>
+      <Typography variant="h5" sx={{ mb: 2, px: 3 }}>{title}</Typography>
+      <Box sx={{ overflowY: 'auto', height: '80vh', px: 3 }}>
+
+        {tasks.map((task) => (
+          <KanbanCard key={task.id} task={task} />
+        ))}
+      </Box>
+    </Box >
   );
 }
