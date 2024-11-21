c3f27ed690867b8e5d41138a477ba68205246c46
Max Oliveira <max-oliveira@outlook.com.br>
2024-11-21 09:20:43 -0300

    more requests

---
+++ src/services/situationEnergyService.js
@@ -0,0 +1,44 @@
+import apiClient from "./apiClient";
+
+const situationEnergyService = {
+  index: async () => {
+    try {
+      const response = await apiClient.get('/api/situation-energy-companies/');
+      return response.data;
+    } catch (error) {
+      console.error('Erro ao buscar solicitação:', error);
+      throw error;
+    }
+  },
+
+  findOne: async (id) => {
+    try {
+      const response = await apiClient.get(`/api/situation-energy-companies/${id}/`);
+      return response.data;
+    } catch (error) {
+      console.error(`Erro ao buscar solicitação com id ${id}:`, error);
+      throw error;
+    }
+  },
+  update: async (id, data) => {
+    try {
+      const response = await apiClient.patch(`/api/situation-energy-companies/${id}/`, data);
+      return response.data;
+    } catch (error) {
+      console.error(`Erro ao atualizar solicitação com id ${id}:`, error);
+      throw error;
+    }
+  },
+
+  create: async (data) => {
+    try {
+      const response = await apiClient.post('/api/situation-energy-companies/', data);
+      return response.data;
+    } catch (error) {
+      console.error('Erro ao criar solicitação:', error);
+      throw error;
+    }
+  },
+};
+
+export default situationEnergyService;
