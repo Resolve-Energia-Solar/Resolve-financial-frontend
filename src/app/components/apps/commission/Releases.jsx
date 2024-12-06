6071dfccd97625c761f54c592d5dd1a6d15471da
Max Oliveira <max-oliveira@outlook.com.br>
2024-12-06 16:56:45 -0300

    fix: add intatment

--- src/app/components/apps/commission/Releases.jsx
+++ src/app/components/apps/commission/Releases.jsx
@@ -61,7 +61,7 @@ function Releases({ data }) {
           <TableBody>
             {DataReleases.map((row) => (
               <TableRow
-                key={row.name}
+                key={row.id}
                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
               >
                 <TableCell align="right">{row.status}</TableCell>
