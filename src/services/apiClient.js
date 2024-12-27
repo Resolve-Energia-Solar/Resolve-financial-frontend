cc582ffbaf099d07776dfc8e2d40efa5339eaedc
Thiago Menezes <thimenezes19@gmail.com>
2024-12-27 17:59:31 -0300

    remove console log

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -2,8 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
-console.log('NEXT ENV:', process.env.NEXT_PUBLIC_API_BASE_URL);
-console.log('API_BASE_URL:', API_BASE_URL);
+
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
 });
@@ -11,26 +10,17 @@ const apiClient = axios.create({
 apiClient.interceptors.request.use(
   (config) => {
     const token = Cookies.get('access_token');
-    console.log('Request config:', config);
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
-      console.log('Token added to headers:', token);
     }
     return config;
   },
-  (error) => {
-    console.error('Request error:', error);
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
 
 apiClient.interceptors.response.use(
-  (response) => {
-    console.log('Response:', response);
-    return response;
-  },
+  (response) => response,
   (error) => {
-    console.error('Response error:', error);
     return Promise.reject(error);
   }
 );
