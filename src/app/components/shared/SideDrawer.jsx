1ddee83b4ff873911539d4fce40e6a14b9e8eae1
Leonardo Costa <leonardoabreu1212@gmail.com>
2024-12-30 15:34:25 -0300

    feat: make SideDrawer responsive

--- src/app/components/shared/SideDrawer.jsx
+++ src/app/components/shared/SideDrawer.jsx
@@ -4,8 +4,13 @@ import ParentCard from "./ParentCard";
 export default function SideDrawer({ title, children, open, onClose }) {
 
     return (
-        <Drawer anchor='right' open={open} onClose={onClose}  sx={{ zIndex: 9999 }}>
-            <Box maxWidth='50vw' minWidth='50vw'>
+        <Drawer anchor='right' open={open} onClose={onClose} >
+            <Box
+                sx={{
+                    width: { xs: '100vw', sm: '100vw' },
+                    maxWidth: { md: '70vw' },
+                    height: '100vh',
+                }} >
                 <ParentCard title={title}  >
                     <CardContent>
                         {children}
