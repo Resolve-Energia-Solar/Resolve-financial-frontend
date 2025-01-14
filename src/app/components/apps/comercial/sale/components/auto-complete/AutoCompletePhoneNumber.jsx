cf089b01edce5dc0b08f9a27e3c5ca394bff01d3
Thiago Menezes <thimenezes19@gmail.com>
2025-01-14 13:35:21 -0300

    feat: modify autocomplete

--- src/app/components/apps/comercial/sale/components/auto-complete/AutoCompletePhoneNumber.jsx
+++ src/app/components/apps/comercial/sale/components/auto-complete/AutoCompletePhoneNumber.jsx
@@ -14,7 +14,7 @@ export default function AutoCompletePhoneNumber({
   value,
   error,
   helperText,
-  disableSuggestions=true,
+  disableSuggestions=false,
   ...props
 }) {
   const [open, setOpen] = useState(false);
