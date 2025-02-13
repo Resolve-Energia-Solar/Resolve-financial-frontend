8dde61759af63a67cb4907b11f1752c6f12a33a4
matheusmacias <matheusmacias3@gmail.com>
2025-02-13 10:20:51 -0300

    fix: pagination list leads

--- src/app/components/kanban/Leads/Leads-list/index.jsx
+++ src/app/components/kanban/Leads/Leads-list/index.jsx
@@ -13,73 +13,41 @@ import {
   Chip
 } from '@mui/material';
 import { Add } from '@mui/icons-material';
-import { useRouter } from 'next/navigation';
-import projectService from '@/services/projectService';
-import StatusChip from '@/utils/status/ProjectStatusChip';
-import DocumentStatusChip from '@/utils/status/DocumentStatusIcon';
-import { ProjectDataContext } from '@/app/context/ProjectContext';
 import TableSkeleton from '@/app/components/apps/comercial/sale/components/TableSkeleton';
-import ChipProject from '@/app/components/apps/project/components/ChipProject';
 import leadService from '@/services/leadService';
+import formatPhoneNumber from '@/utils/formatPhoneNumber';
 
 const LeadList = ({ onClick }) => {
-  // Estados para os dados e loading dos projetos
-  const [projectsList, setProjectsList] = useState([]);
-  const [loadingProjects, setLoadingProjects] = useState(true);
+  const [leadsList, setLeadsList] = useState([]);
+  const [loadingLeads, setLoadingLeads] = useState(true);
 
-  // Estados para os indicadores
-  const [indicators, setIndicators] = useState({});
-  const [loadingIndicators, setLoadingIndicators] = useState(true);
-
-  const formatPhoneNumber = (phone) => {
-    if (!phone) return "-";
-
-    // Remover caracteres não numéricos
-    const cleaned = phone.replace(/\D/g, "");
-
-    // Verificar se o número tem o tamanho esperado
-    if (cleaned.length < 10 || cleaned.length > 11) return phone; // Retorna como está se não for válido
-
-    // Adicionar código do país (Brasil +55 como exemplo)
-    const countryCode = "+55";
-
-    // Extrair DDD e número
-    const ddd = cleaned.slice(0, 2);
-    const firstPart = cleaned.length === 10 ? cleaned.slice(2, 6) : cleaned.slice(2, 7);
-    const secondPart = cleaned.length === 10 ? cleaned.slice(6) : cleaned.slice(7);
-
-    return `${countryCode} (${ddd}) ${firstPart}-${secondPart}`;
-  };
-
-  // Outros estados
   const [error, setError] = useState(null);
   const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
   const [totalRows, setTotalRows] = useState(0);
-  const router = useRouter();
-  const { filters, setFilters, refresh } = useContext(ProjectDataContext);
 
   useEffect(() => {
-    // Função para buscar os projetos
-    const fetchProjects = async () => {
-      setLoadingProjects(true);
+    const fetchLeads = async () => {
+      setLoadingLeads(true);
       try {
         const data = await leadService.getLeads({
-          page: page + 1,
-          limit: rowsPerPage,
+          params: {
+            page: page + 1,
+            limit: rowsPerPage,
+          },
         });
-        setProjectsList(data.results);
+        setLeadsList(data.results);
         setTotalRows(data.count);
       } catch (err) {
-        setError('Erro ao carregar Projetos');
+        setError('Erro ao carregar Leads');
       } finally {
-        setLoadingProjects(false);
+        setLoadingLeads(false);
       }
     };
 
-    fetchProjects();
+    fetchLeads();
 
-  }, [page, rowsPerPage, filters, refresh]);
+  }, [page, rowsPerPage]);
 
   const handlePageChange = (event, newPage) => {
     setPage(newPage);
@@ -94,7 +62,7 @@ const LeadList = ({ onClick }) => {
     <>
       <Box sx={{ display: 'flex', justifyContent: 'end', alignItems: 'flex-end' }}>
         <Box>
-          {/* Exemplo: Botão para criar projeto */}
+          {/* Exemplo: Botão para criar lead */}
           <Button
             startIcon={<Add />}
             // onClick={() => router.push('/apps/lead/create')}
@@ -127,34 +95,34 @@ const LeadList = ({ onClick }) => {
               <TableCell sx={{ fontWeight: 600, fontSize: "13px" }}>Nome</TableCell>
               <TableCell sx={{ fontWeight: 600, fontSize: "13px" }}>CPF/CNPJ</TableCell>
               <TableCell sx={{ fontWeight: 600, fontSize: "13px" }}>Origem</TableCell>
-              <TableCell sx={{ fontWeight: 600, fontSize: "13px" }}>KwH</TableCell>
+              <TableCell sx={{ fontWeight: 600, fontSize: "13px" }}>kWp</TableCell>
               <TableCell sx={{ fontWeight: 600, fontSize: "13px" }}>Endereço</TableCell>
               <TableCell sx={{ fontWeight: 600, fontSize: "13px" }}>Fone</TableCell>
               <TableCell sx={{ fontWeight: 600, fontSize: "13px" }}>Status</TableCell>
             </TableRow>
           </TableHead>
-          {loadingProjects ? (
+          {loadingLeads ? (
             <TableSkeleton rows={rowsPerPage} cols={7} />
           ) : error && page === 1 ? (
             <Typography color="error">{error}</Typography>
           ) : (
             <TableBody>
-              {projectsList.map((item) => (
+              {leadsList.map((item) => (
                 <TableRow
                   key={item.id}
-                  onClick={() => onClick(item)}
+                  // onClick={() => onClick(item)}
                   sx={{
                     '&:hover': {
                       backgroundColor: 'rgba(236, 242, 255, 0.35)',
                     },
                   }}
                 >
-                  <TableCell sx={{fontWeight: "600", color: "#7E8388"}}>{item?.name}</TableCell>
-                  <TableCell sx={{color: "#7E8388"}}>{item.first_document || '-'}</TableCell>
-                  <TableCell sx={{color: "#7E8388"}}>{item?.origin?.name || '-'}</TableCell>
-                  <TableCell sx={{color: "#7E8388"}}>{item?.kwp || '-'}</TableCell>
-                  <TableCell sx={{color: "#7E8388"}}>{item?.addresses[0]?.street || '-'}, {item?.addresses[0]?.number || '-'} - {item?.addresses[0]?.city || '-'}</TableCell>
-                  <TableCell sx={{color: "#7E8388"}}>{formatPhoneNumber(item?.phone)}</TableCell>
+                  <TableCell sx={{ fontWeight: "600", color: "#7E8388" }}>{item?.name}</TableCell>
+                  <TableCell sx={{ color: "#7E8388" }}>{item.first_document || '-'}</TableCell>
+                  <TableCell sx={{ color: "#7E8388" }}>{item?.origin?.name || '-'}</TableCell>
+                  <TableCell sx={{ color: "#7E8388" }}>{item?.kwp || '-'}</TableCell>
+                  <TableCell sx={{ color: "#7E8388" }}>{item?.addresses[0]?.street || '-'}, {item?.addresses[0]?.number || '-'} - {item?.addresses[0]?.city || '-'}</TableCell>
+                  <TableCell sx={{ color: "#7E8388" }}>{formatPhoneNumber(item?.phone)}</TableCell>
                   <TableCell>
                     <Chip
                       label={item?.column?.name || '-'}
