a4c57d8d5f82896c759eb2ec5a09a30e2678367f
Leonardo Costa <leonardoabreu1212@gmail.com>
2025-02-27 12:24:54 -0300

    refactor: display customer name at AutoCompleteProject

--- src/app/components/apps/inspections/auto-complete/Auto-input-Project.jsx
+++ src/app/components/apps/inspections/auto-complete/Auto-input-Project.jsx
@@ -1,10 +1,10 @@
 'use client';
 import { Fragment, useCallback, useEffect, useState } from 'react';
-
 import Autocomplete from '@mui/material/Autocomplete';
 import CircularProgress from '@mui/material/CircularProgress';
 import CustomTextField from '@/app/components/forms/theme-elements/CustomTextField';
 import projectService from '@/services/projectService';
+import userService from '@/services/userService';
 import { debounce } from 'lodash';
 
 export default function AutoCompleteProject({ onChange, value, error, helperText }) {
@@ -19,9 +19,17 @@ export default function AutoCompleteProject({ onChange, value, error, helperText
         try {
           const projectValue = await projectService.getProjectById(value);
           if (projectValue) {
+            let customerName = '';
+            try {
+              const user = await userService.getUserById(projectValue.sale.customer);
+              customerName = user.complete_name;
+            } catch (err) {
+              console.error('Erro ao buscar usuário:', err);
+            }
             setSelectedProject({
               id: projectValue.id,
               project_number: projectValue.project_number,
+              customerName,
             });
           }
         } catch (error) {
@@ -29,17 +37,12 @@ export default function AutoCompleteProject({ onChange, value, error, helperText
         }
       }
     };
-
     fetchDefaultProject();
   }, [value]);
 
   const handleChange = (event, newValue) => {
     setSelectedProject(newValue);
-    if (newValue) {
-      onChange(newValue.id);
-    } else {
-      onChange(null);
-    }
+    onChange(newValue ? newValue.id : null);
   };
 
   const fetchProjectsByCodeNumber = useCallback(
@@ -47,28 +50,36 @@ export default function AutoCompleteProject({ onChange, value, error, helperText
       setLoading(true);
       try {
         const response = await projectService.getProjects();
-        const formattedProjects = response.results.results
-          .filter((project) =>
-            project.project_number.toLowerCase().includes(codeNumber.toLowerCase()),
-          )
-          .map((project) => ({
-            id: project.id,
-            project_number: project.project_number,
-          }));
-        console.log('formattedProjects: ', formattedProjects);
+        const filteredProjects = response.results.results.filter((project) =>
+          project.project_number.toLowerCase().includes(codeNumber.toLowerCase())
+        );
+
+        const formattedProjects = await Promise.all(
+          filteredProjects.map(async (project) => {
+            let customerName = '';
+            try {
+              const user = await userService.getUserById(project.sale.customer);
+              customerName = user.complete_name;
+            } catch (error) {
+              console.error('Erro ao buscar usuário:', error);
+            }
+            return {
+              id: project.id,
+              project_number: project.project_number,
+              customerName,
+            };
+          })
+        );
         setOptions(formattedProjects);
       } catch (error) {
         console.error('Erro ao buscar projetos:', error);
       }
       setLoading(false);
     }, 300),
-    [],
+    []
   );
 
-  const handleOpen = () => {
-    setOpen(true);
-  };
-
+  const handleOpen = () => setOpen(true);
   const handleClose = () => {
     setOpen(false);
     setOptions([]);
@@ -82,7 +93,9 @@ export default function AutoCompleteProject({ onChange, value, error, helperText
         onOpen={handleOpen}
         onClose={handleClose}
         isOptionEqualToValue={(option, value) => option.project_number === value.project_number}
-        getOptionLabel={(option) => option.project_number || ''}
+        getOptionLabel={(option) =>
+          `${option.project_number} - ${option.customerName || ''}`
+        }
         options={options}
         loading={loading}
         value={selectedProject}
