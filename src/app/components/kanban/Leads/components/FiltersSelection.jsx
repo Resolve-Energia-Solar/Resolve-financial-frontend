bab6e1028c33bb3482b5518953197d544e80f62e
manu <manuelatourinhodeoliveira@gmail.com>
2025-03-06 17:31:34 -0300

    filters with style according to figma

--- src/app/components/kanban/Leads/components/FiltersSelection.jsx
+++ src/app/components/kanban/Leads/components/FiltersSelection.jsx
@@ -1,34 +1,31 @@
-import { Box, Typography, MenuItem, Select } from "@mui/material";
-import { ExpandMore } from "@mui/icons-material"; // Custom dropdown icon
+import { Box, MenuItem, Select } from "@mui/material";
+import { ExpandMore } from "@mui/icons-material"; 
 
 const FilterSelect = ({ label, options, value, onChange }) => {
   return (
-    <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
-      <Typography sx={{ color: "#7E8388", fontSize: "14px", fontWeight: 500 }}>
-        {label}
-      </Typography>
-      <Select
-        value={value}
-        onChange={onChange}
-        displayEmpty
-        IconComponent={ExpandMore} 
-        sx={{
-          fontSize: "14px",
-          color: "#7E8388",
-          backgroundColor: "transparent",
-          border: "none",
-          "&:hover": { backgroundColor: "transparent" },
-          "& .MuiOutlinedInput-notchedOutline": { border: "none" },
-        }}
-      >
-        <MenuItem value="">Todos</MenuItem>
-        {options.map((option) => (
-          <MenuItem key={option.value} value={option.value}>
-            {option.label}
-          </MenuItem>
-        ))}
-      </Select>
-    </Box>
+    <Select
+      value={value}
+      onChange={onChange}
+      displayEmpty
+      IconComponent={ExpandMore} 
+      renderValue={(selected) => (selected ? options.find(opt => opt.value === selected)?.label : label)}
+      sx={{
+        fontSize: "12px",
+        color: "#7E8388",
+        backgroundColor: "transparent",
+        border: "none",
+        fontWeight: 500,
+        "&:hover": { backgroundColor: "transparent" },
+        "& .MuiOutlinedInput-notchedOutline": { border: "none" }, 
+      }}
+    >
+      <MenuItem disabled>{label}</MenuItem> 
+      {options.map((option) => (
+        <MenuItem key={option.value} value={option.value}>
+          {option.label}
+        </MenuItem>
+      ))}
+    </Select>
   );
 };
 
