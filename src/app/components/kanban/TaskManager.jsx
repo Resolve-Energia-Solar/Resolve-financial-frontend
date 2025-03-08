f23a9e257017c2010abf09478d15ed39a04fa006
manu <manuelatourinhodeoliveira@gmail.com>
2025-03-07 21:07:31 -0300

    changed simplebar to box as simplebar component so it accepts the style for horizontal scrollbar

--- src/app/components/kanban/TaskManager.jsx
+++ src/app/components/kanban/TaskManager.jsx
@@ -7,6 +7,7 @@ import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 import SimpleBar from 'simplebar-react';
 import { Box } from '@mui/material';
 import CategoryTaskListSkeleton from './components/CategoryTaskListSkeleton';
+import { debounce } from 'lodash';
 
 function TaskManager() {
   const { todoCategories, loadingCategories, moveTask } = useContext(KanbanDataContext);
@@ -27,10 +28,32 @@ function TaskManager() {
     moveTask(draggableId, sourceCategoryId, destinationCategoryId, sourceIndex, destinationIndex);
   };
 
+  const handleScroll = debounce((event) => {
+    if (loading) return;
+
+    const { scrollTop, scrollHeight, clientHeight } = event.target;
+    const scrollPosition = scrollTop + clientHeight;
+
+    const isNearTop = scrollPosition <= 0.35 * scrollHeight && page > 1;
+
+    const isNearBottom = scrollPosition >= 0.75 * scrollHeight && hasNext;
+
+    console.log('isNearTop:', isNearTop);
+    console.log('page:', page);
+
+    if (isNearTop && !loading) {
+      setPage(1);
+    }
+
+    if (isNearBottom) {
+      nextPage();
+    }
+  }, 700);
+
   return (
     <>
       <KanbanHeader />
-      <SimpleBar
+      {/* <SimpleBar
         style={{
           maxWidth: '100%',
           overflowX: 'auto',
@@ -38,12 +61,39 @@ function TaskManager() {
         }}
         forceVisible="x"
         autoHide={false}
+      > */}
+      <Box
+
+        component={SimpleBar}
+        overflow="auto"
+        px={3}
+        maxHeight="calc(100vh - 160px)"
+        onScroll={handleScroll} // Adiciona o evento de rolagem
+        forceVisible="x"
+        autoHide={false}
+        sx={{
+          maxWidth: '100%',
+          overflowX: 'auto',
+          whiteSpace: 'nowrap',
+          '& .simplebar-scrollbar::before': {
+            backgroundColor: '#7E8388 !important',
+            opacity: '1 !important',
+            borderRadius: '8px',
+          },
+          '& .simplebar-track': {
+            backgroundColor: '#D9D9D9 !important',
+            borderRadius: '8px',
+          },
+          '& .simplebar-track.simplebar-vertical': {
+            backgroundColor: '#ccc !important',
+          },
+        }}
       >
         <DragDropContext onDragEnd={onDragEnd}>
-          <Box display="flex" gap={2} p={1} 
+          <Box display="flex" gap={2} p={1}
             sx={{
               minWidth: 'max-content',
-              maxHeight: '75vh', 
+              maxHeight: '75vh',
             }}
           >
             {loadingCategories
@@ -62,7 +112,8 @@ function TaskManager() {
               ))}
           </Box>
         </DragDropContext>
-      </SimpleBar>
+      </Box>
+      {/* </SimpleBar> */}
     </>
   );
 }
