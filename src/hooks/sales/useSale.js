a70eec4ebc0e1feb614470369b6a3ee65eca7dac
Max Oliveira <max-oliveira@outlook.com.br>
2025-03-10 03:14:47 -0300

    refactor: remove console, add param

--- src/hooks/sales/useSale.js
+++ src/hooks/sales/useSale.js
@@ -1,7 +1,11 @@
 import { useState, useEffect, useCallback } from 'react';
 import saleService from '@/services/saleService';
+import { useRouter } from "next/navigation";
 
 const useSale = (id) => {
+
+  const router = useRouter();
+
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [saleData, setSaleData] = useState(null);
@@ -11,19 +15,56 @@ const useSale = (id) => {
   const handleRowClick = (item) => {
     setOpenDrawer(true);
     setRowSelected(item);
-    console.log(item);
+    router.push(`?id=${item.id}`);
+
   };
 
   const toggleDrawerClosed = () => {
     setOpenDrawer(false);
     setRowSelected(null);
+    router.replace("/apps/commercial/sale?", { scroll: false });
   };
 
   const fetchSale = useCallback(async () => {
     if (!id) return;
 
     try {
-      const data = await saleService.getSaleById(id);
+      const data = await saleService.find(id,
+        {
+          expand: [
+            "attachments",
+            "payments",
+            "contract-submissions",
+            "customer.addresses",
+            "projects.units",
+            "comments"
+          ],
+          fields: [
+            "customer.id",
+            "customer.complete_name",
+            "customer.email",
+            "customer.first_document",
+            "customer.birth_date",
+            "id",
+            "total_value",
+            "branch.id",
+            "branch.name",
+            "marketing_campaign",
+            "seller",
+            "sales_supervisor.id",
+            "sales_supervisor.complete_name",
+            "sales_manager.id",
+            "sales_manager.complete_name",
+            "payment_status",
+            "status",
+            "billing_date",
+            "cancellation_reasons",
+            "is_pre_sale"
+          ],
+          format: "json",
+          limit: 1
+        }
+      );
       setSaleData(data);
     } catch (err) {
       setError('Erro ao carregar a venda');
