c9bfb2696c008d00347a8c08274ed8f12b4f8b58
manu <manuelatourinhodeoliveira@gmail.com>
2025-03-11 09:50:49 -0300

    added displayEmpty to keep the disabled option from showing up in the dropdown

--- src/app/components/kanban/Leads/components/HeaderCard.jsx
+++ src/app/components/kanban/Leads/components/HeaderCard.jsx
@@ -190,39 +190,40 @@ function LeadInfoHeader({ leadId, tabValue }) {
               fullWidth
               size="small"
               sx={{ backgroundColor: '#F4F5F7', borderRadius: '8px',  }}
+              displayEmpty
             >
               {/* {projects.map((project) => ( */}
-              <MenuItem
-                value=""
-                sx={{ color: '#7E8388' }}
-                disabled
-              >
-                Selecione um projeto
-              </MenuItem>
-
-                <MenuItem 
-                  // key={project.id} 
-                  // value={project.id} 
-                  value="project-1"
+                <MenuItem
+                  value=""
                   sx={{ color: '#7E8388' }}
+                  disabled
                 >
-                  {/* {project.name} */}
-                  Rua Antônio Barreto, 1198
+                  Selecione um projeto
                 </MenuItem>
 
-                <MenuItem 
-                  value="project-2"
-                  sx={{ color: '#7E8388' }}
-                >
-                  Rua dos Mundurucus, 2500
-                </MenuItem>
+                  <MenuItem 
+                    // key={project.id} 
+                    // value={project.id} 
+                    value="project-1"
+                    sx={{ color: '#7E8388' }}
+                  >
+                    {/* {project.name} */}
+                    Rua Antônio Barreto, 1198
+                  </MenuItem>
 
-                <MenuItem 
-                  value="project-3"
-                  sx={{ color: '#7E8388' }}
-                >
-                  Tv. Padre Eutíquio, 87
-                </MenuItem>
+                  <MenuItem 
+                    value="project-2"
+                    sx={{ color: '#7E8388' }}
+                  >
+                    Rua dos Mundurucus, 2500
+                  </MenuItem>
+
+                  <MenuItem 
+                    value="project-3"
+                    sx={{ color: '#7E8388' }}
+                  >
+                    Tv. Padre Eutíquio, 87
+                  </MenuItem>
 
               {/* ))} */}
             </Select>
