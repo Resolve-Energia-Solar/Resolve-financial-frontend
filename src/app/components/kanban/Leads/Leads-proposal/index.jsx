deebc88794a657955e1de7917601a9a61d7ca6b9
manu <manuelatourinhodeoliveira@gmail.com>
2025-03-13 10:23:03 -0300

    changed opening method from dialog to drawer

--- src/app/components/kanban/Leads/Leads-proposal/index.jsx
+++ src/app/components/kanban/Leads/Leads-proposal/index.jsx
@@ -199,20 +199,26 @@ const LeadsProposalListPage = ({ leadId = null }) => {
                                 </DialogContent>
                             </Drawer>
 
-                            <Dialog
+                            <Drawer
+                                anchor="right"
                                 open={openEditProposal}
                                 onClose={() => setOpenEditProposal(false)}
-                                maxWidth="lg"
-                                fullWidth
+                                // maxWidth="lg"
+                                sx={{ zIndex: 1300 }}
+                                PaperProps={{
+                                    sx: {
+                                        width: "60vw",
+                                        p: 4,
+                                    }
+                                }}
                             >
                                 <DialogContent>
-                                    {/* add a new edit page in the future */}
                                     <EditProposalPage 
                                         leadId={leadId} 
                                         onClose={() => setOpenEditProposal(false)} 
                                         onRefresh={handleRefresh} />
                                 </DialogContent>
-                            </Dialog>
+                            </Drawer>
 
                             <Dialog
                                 open={openDetailProposal}
