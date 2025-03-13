cdedf9bbc92df1960375db859dd05ddf3e6ee7a6
Max Oliveira <max-oliveira@outlook.com.br>
2025-03-12 23:40:33 -0300

    feat: new component DetailsTab

--- src/app/components/apps/sale/DetailsTabs.jsx
+++ src/app/components/apps/sale/DetailsTabs.jsx
@@ -1,23 +1,108 @@
 'use client';
-import { Tabs, Tab, Box } from '@mui/material';
-import CustomerTabs from '@/app/components/apps/users/Edit-user/customer/tabs';
-import { useState } from 'react';
-import GeneralInfoTab from './GeneralInfoTab';
+import { Tabs, Tab, Box, CircularProgress } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { TabPanel } from '../../shared/TabPanel';
+import saleService from '@/services/saleService';
+import Addresses from './Adresses';
+import Phones from './phones';
+import Customer from './Customer';
 
-const CONTEXT_TYPE_SALE_ID = process.env.NEXT_PUBLIC_CONTENT_TYPE_SALE_ID;
-
-const DetailsTabs = ({ id, ...props }) => {
+const DetailsTabs = ({ id, onRefresh, ...props }) => {
   const [value, setValue] = useState(0);
 
   const handleChangeTab = (event, newValue) => {
     setValue(newValue);
   };
+
+  // =========
+
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState();
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    fetchSale();
+  }, []);
+
+  const fetchSale = async () => {
+    try {
+      const response = await saleService.find(id, {
+        expand: [
+          'attachments',
+          'sales_supervisor',
+          'sales_manager',
+          'branch',
+          'payments',
+          'contract-submissions',
+          'customer',
+          'customer.phone_numbers',
+          'customer.addresses',
+          'projects',
+          'projects.units',
+          'comments',
+          'marketing_campaign',
+          'seller',
+        ],
+        fields: [
+          'customer.id',
+          'customer.complete_name',
+          'customer.email',
+          'customer.first_document',
+          'customer.birth_date',
+          'customer.phone_numbers.id',
+          'customer.phone_numbers.area_code',
+          'customer.phone_numbers.phone_number',
+          'customer.phone_numbers.is_main',
+          'customer.addresses',
+          'customer.gender',
+          'customer.person_type',
+          'id',
+          'total_value',
+          'branch.id',
+          'branch.name',
+          'marketing_campaign.id',
+          'marketing_campaign.name',
+          'seller.id',
+          'seller.complete_name',
+          'sales_supervisor.id',
+          'sales_supervisor.complete_name',
+          'sales_manager.id',
+          'sales_manager.complete_name',
+          'payment_status',
+          'status',
+          'billing_date',
+          'cancellation_reasons',
+          'is_pre_sale',
+          'attachments.id',
+          'attachments.document_type.id',
+          'attachments.document_type.name',
+          'attachments.status',
+          'attachments.object_id',
+          'attachments.document_subtype',
+          'attachments.file',
+          'attachments.description',
+          'attachments.created_at',
+        ],
+        format: 'json',
+      });
+
+      setData(response);
+    } catch (error) {
+      console.log(error);
+      setError(error);
+      throw error;
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <Box {...props}>
       <Tabs value={value} onChange={handleChangeTab} variant="scrollable" scrollButtons="auto">
-        <Tab label="Cliente" />
-        <Tab label="Vistoria" />
+        <Tab label="Gerais" />
+        <Tab label="Vistorias" />
         <Tab label="Venda" />
+        <Tab label="Projetos" />
         <Tab label="Anexos" />
         <Tab label="Pagamentos" />
         <Tab label="Checklist" />
@@ -26,20 +111,50 @@ const DetailsTabs = ({ id, ...props }) => {
         <Tab label="Comentários" />
       </Tabs>
 
-      <Box>
-        {value === 0 && (
-          <Box sx={{ mt: 3 }}>
-            <GeneralInfoTab data={''} />
-          </Box>
-        )}
-        {value === 1 && <Box sx={{ mt: 3 }}></Box>}
-        {value === 2 && <Box sx={{ mt: 3 }}></Box>}
-        {value === 3 && <Box sx={{ mt: 3 }}></Box>}
-        {value === 4 && <Box sx={{ mt: 3 }}></Box>}
-        {value === 5 && <Box sx={{ mt: 3 }}></Box>}
-        {value === 6 && <Box sx={{ mt: 3 }}></Box>}
-        {value === 7 && <Box sx={{ mt: 3 }}></Box>}
-      </Box>
+      {error && <Box>Erro</Box>}
+
+      {data ? (
+        <Box sx={{ overflowY: 'auto', height: '90vh' }}>
+          <TabPanel value={value} index={0}>
+            <Box
+              sx={{
+                bgcolor: 'background.paper',
+                display: 'flex',
+                padding: 0,
+                flexDirection: 'column',
+              }}
+            >
+              <Customer data={data.customer} onRefresh={onRefresh} />
+              <Phones
+                data={data.customer.phone_numbers}
+                onRefresh={fetchSale}
+                userId={data.customer.id}
+              />
+              <Addresses
+                data={data.customer.addresses}
+                onRefresh={fetchSale}
+                userId={data.customer.id}
+              />
+            </Box>
+          </TabPanel>
+          <TabPanel value={value} index={1}></TabPanel>
+          <TabPanel value={value} index={2}></TabPanel>
+          <TabPanel value={value} index={3}></TabPanel>
+          <TabPanel value={value} index={4}></TabPanel>
+        </Box>
+      ) : (
+        <Box
+          sx={{
+            width: '100%',
+            height: '70vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <CircularProgress size={40} color="inherit" />
+        </Box>
+      )}
     </Box>
   );
 };
