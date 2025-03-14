f072f7942da92cce2fb691d4dd6979a3a21fedb2
manu <manuelatourinhodeoliveira@gmail.com>
2025-03-14 10:04:44 -0300

    created conditional for 10 as value

--- src/app/components/kanban/Leads/components/HeaderCard.jsx
+++ src/app/components/kanban/Leads/components/HeaderCard.jsx
@@ -189,46 +189,98 @@ function LeadInfoHeader({ leadId, tabValue }) {
               onChange={(e) => setSelectedProject(e.target.value)}
               fullWidth
               size="small"
-              sx={{ backgroundColor: '#F4F5F7', borderRadius: '8px',  }}
+              sx={{ backgroundColor: '#F4F5F7', borderRadius: '8px', }}
               displayEmpty
             >
               {/* {projects.map((project) => ( */}
-                <MenuItem
-                  value=""
-                  sx={{ color: '#7E8388' }}
-                  disabled
-                >
-                  Selecione um projeto
-                </MenuItem>
+              <MenuItem
+                value=""
+                sx={{ color: '#7E8388' }}
+                disabled
+              >
+                Selecione um projeto
+              </MenuItem>
 
-                  <MenuItem 
-                    // key={project.id} 
-                    // value={project.id} 
-                    value="project-1"
-                    sx={{ color: '#7E8388' }}
-                  >
-                    {/* {project.name} */}
-                    Rua Antônio Barreto, 1198
-                  </MenuItem>
+              <MenuItem
+                // key={project.id} 
+                // value={project.id} 
+                value="project-1"
+                sx={{ color: '#7E8388' }}
+              >
+                {/* {project.name} */}
+                Rua Antônio Barreto, 1198
+              </MenuItem>
 
-                  <MenuItem 
-                    value="project-2"
-                    sx={{ color: '#7E8388' }}
-                  >
-                    Rua dos Mundurucus, 2500
-                  </MenuItem>
+              <MenuItem
+                value="project-2"
+                sx={{ color: '#7E8388' }}
+              >
+                Rua dos Mundurucus, 2500
+              </MenuItem>
 
-                  <MenuItem 
-                    value="project-3"
-                    sx={{ color: '#7E8388' }}
-                  >
-                    Tv. Padre Eutíquio, 87
-                  </MenuItem>
+              <MenuItem
+                value="project-3"
+                sx={{ color: '#7E8388' }}
+              >
+                Tv. Padre Eutíquio, 87
+              </MenuItem>
 
               {/* ))} */}
             </Select>
           </Box>
         )}
+
+        {tabValue === 10 && (
+          <Box sx={{ minWidth: 385 }}>
+            <Typography variant="caption" sx={{ color: 'gray', mb: 0.5 }}>
+              Projeto
+            </Typography>
+            <Select
+              value={selectedProject || ''}
+              onChange={(e) => setSelectedProject(e.target.value)}
+              fullWidth
+              size="small"
+              sx={{ backgroundColor: '#F4F5F7', borderRadius: '8px', }}
+              displayEmpty
+            >
+              {/* {projects.map((project) => ( */}
+              <MenuItem
+                value=""
+                sx={{ color: '#7E8388' }}
+                disabled
+              >
+                Selecione um projeto
+              </MenuItem>
+
+              <MenuItem
+                // key={project.id} 
+                // value={project.id} 
+                value="project-1"
+                sx={{ color: '#7E8388' }}
+              >
+                {/* {project.name} */}
+                Rua Antônio Barreto, 1198
+              </MenuItem>
+
+              <MenuItem
+                value="project-2"
+                sx={{ color: '#7E8388' }}
+              >
+                Rua dos Mundurucus, 2500
+              </MenuItem>
+
+              <MenuItem
+                value="project-3"
+                sx={{ color: '#7E8388' }}
+              >
+                Tv. Padre Eutíquio, 87
+              </MenuItem>
+
+              {/* ))} */}
+            </Select>
+          </Box>
+        )}
+
       </Grid>
     </Box>
   );
