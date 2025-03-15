f1614bfd9cc3b6930c01a17011cad14bb8cb85d0
manu <manuelatourinhodeoliveira@gmail.com>
2025-03-15 02:13:09 -0300

    applied leads index page logic to firsstly use the tabs

--- src/app/components/kanban/Leads/Leads-sales/View-Sale.jsx
+++ src/app/components/kanban/Leads/Leads-sales/View-Sale.jsx
@@ -1,130 +1,136 @@
 'use client';
-import {
-  Grid,
-  Typography,
-  Box,
-  useTheme,
-  Button,
-  Card,
-  TableContainer,
-  Table,
-  TableHead,
-  TableCell,
-  TableBody,
-  Paper,
-  TableRow,
-} from '@mui/material';
 
-import { useState } from 'react';
-import { useSnackbar } from 'notistack';
-import { useRouter } from 'next/navigation';
-import LeadInfoHeader from '@/app/components/kanban/Leads/components/HeaderCard';
-import { useSelector, useDispatch } from 'react-redux';
-import useProposal from '@/hooks/proposal/useProposal';
-import { PictureAsPdfTwoTone, TaskAlt, ThumbDownOffAlt } from '@mui/icons-material';
-import ProposalService from '@/services/proposalService';
 
-function LeadsViewSale({ leadId = null, proposalId = null, onClose=null, onRefresh=null }) {
-  const router = useRouter();
-  const theme = useTheme();
-  const { enqueueSnackbar } = useSnackbar();
-  const user = useSelector((state) => state.user);
-  const dispatch = useDispatch();
+import React, { useState } from 'react';
+import { Tabs, Tab, Box, useTheme, } from '@mui/material';
+import PropTypes from 'prop-types';
+import EditLeadPage from '../Leads/Edit-Lead';
+import EditCustomerPage from '../Leads-customer/Edit-Customer';
+import LeadsProposalListPage from '../Leads-proposal';
+import SalesListPage from '.';
+import LeadDocumentPage from '../Leads-documents';
+import LeadSchedulePage from '../Leads-schedule';
 
-  const { proposalData } = useProposal(proposalId);
 
-  const proposalStatus = {
-    "A": { label: "Aceita", color: "#E9F9E6" },
-    "R": { label: "Recusada", color: "#FEEFEE" },
-    "P": { label: "Pendente", color: "#FFF7E5" },
-  };
-
-  const formatCurrency = (value) => {
-    return new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(value);
-  };
-
-  const handleUpdateProposal = async (status) => {
-    try {
-      await ProposalService.updateProposalPartial(proposalId, { status });
-      enqueueSnackbar(`Proposta ${status === 'A' ? 'aceita' : 'recusada'} com sucesso!`, { variant: 'success' });
-      if (onRefresh) onRefresh();
-      if (onClose) onClose();
-    } catch (error) {
-      enqueueSnackbar('Erro ao atualizar a proposta.', { variant: 'error' });
-    }
-  };
+function CustomTabPanel(props) {
+  const { children, value, index, ...other } = props;
 
   return (
-    <Grid container spacing={3}>
-      <Grid item xs={12}>
-        <LeadInfoHeader leadId={leadId} tabValue={20} />
-      </Grid>
-
-      <Grid item xs={12}>
-        <Typography variant="h6" sx={{ fontWeight: 800, fontSize: '18px' }}>
-          Contrato - Kit Solar 2034
-        </Typography>
-      </Grid>
-
-      <Grid item xs={12} md={6}>
-        <Grid container spacing={2}>
-          <Grid item xs={4}>
-            <Typography variant="h6" sx={{ fontWeight: 800, fontSize: '14px' }}>
-              Valor da proposta
-            </Typography>
-            <Typography variant="body1" sx={{ fontSize: '14px', color: '#7E92A2', marginTop: 1 }}>
-              {formatCurrency(proposalData?.value || 0)}
-            </Typography>
-          </Grid>
-          <Grid item xs={4}>
-            <Typography variant="h6" sx={{ fontWeight: 800, fontSize: '14px' }}>
-              Status
-            </Typography>
-            <Typography variant="body1" sx={{ fontSize: '14px', color: '#7E92A2', marginTop: 1 }}>
-              {proposalStatus[proposalData?.status]?.label}
-            </Typography>
-          </Grid>
-          <Grid item xs={4}>
-            <Typography variant="h6" sx={{ fontWeight: 800, fontSize: '14px' }}>
-              Data do vencimento
-            </Typography>
-            <Typography variant="body1" sx={{ fontSize: '14px', color: '#7E92A2', marginTop: 1 }}>
-              {new Date(proposalData?.due_date).toLocaleDateString('pt-BR')}
-            </Typography>
-          </Grid>
-        </Grid>
-      </Grid>
-
-
-      <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', mt: 2, gap: 2 }}>
-        <Box>
-          <Button variant="contained" sx={{
-            backgroundColor: 'transparent', color: '#303030', p: 1.2, border: '1px solid #303030', fontSize: '14px',
-          }} startIcon={<PictureAsPdfTwoTone />}>
-            <Typography variant="body1">Visualizar PDF</Typography>
-          </Button>
-        </Box>
-        <Box sx={{ display: 'flex', gap: 2 }}>
-          <Button
-            variant="contained"
-            sx={{ backgroundColor: '#FFEBE4', color: '#FF532E', fontSize: '14px', p: 1.2 }}
-            endIcon={<ThumbDownOffAlt />}
-            onClick={() => handleUpdateProposal('R')}
-          >
-            <Typography variant="body1">Proposta recusada</Typography>
-          </Button>
-          <Button
-            variant="contained"
-            sx={{ backgroundColor: '#FFCC00', color: 'black', fontSize: '14px', p: 1.2 }}
-            endIcon={<TaskAlt />}
-            onClick={() => handleUpdateProposal('A')}
-          >
-            <Typography variant="body1">Proposta fechada</Typography>
-          </Button>
-        </Box>
-      </Grid>
-    </Grid>
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+      }}
+    >
+      {value === index && <Box sx={{ p: 0.1, height: '100%', overflowY: 'auto' }}>{children}</Box>}
+    </div>
   );
 }
+
+CustomTabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+function LeadsViewSale({ leadId }) {
+  const [tabValue, setTabValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setTabValue(newValue);
+  };
+
+  const theme = useTheme();
+
+  return (
+    <Box sx={{ overflow: 'hidden', height: '100%', p: 0, margin: 0 }}>
+
+      <Tabs
+        value={tabValue}
+        onChange={handleChange}
+        aria-label="lead edit tabs"
+        TabIndicatorProps={{ style: { display: 'none' } }}
+        sx={{ marginLeft: '25px', marginBottom: "-1px" }}
+      >
+        {["Informações Lead", "Dados Pessoais", "Propostas", "Vendas", "Documentos", "Projetos", "Agendamentos"].map((label, index) => (
+          <Tab
+            key={index}
+            label={label}
+            {...a11yProps(index)}
+            sx={{
+              backgroundColor: tabValue === index ? theme.palette.primary.main : 'transparent',
+              color: '#303030',
+              fontWeight: 600,
+              fontSize: '12px',
+              borderTopLeftRadius: '10px',
+              borderTopRightRadius: '10px',
+              '&.Mui-selected': {
+                backgroundColor: theme.palette.primary.main,
+                color: '#303030',
+                fontWeight: 600,
+                fontSize: '12px',
+              },
+              '&:not(.Mui-selected)': {
+                fontWeight: 600,
+                fontSize: '12px',
+                color: '#7E8388', 
+              },
+            }}
+          />
+        ))}
+      </Tabs>
+
+          {/* infos do lead */}
+      <CustomTabPanel value={tabValue} index={0}>
+        <EditLeadPage leadId={leadId} />
+      </CustomTabPanel>
+
+          {/* dados pessoais */}
+      <CustomTabPanel value={tabValue} index={1}>
+        <EditCustomerPage leadId={leadId} />
+      </CustomTabPanel>
+
+          {/* propostas */}
+      <CustomTabPanel value={tabValue} index={2}>
+        <LeadsProposalListPage leadId={leadId} />
+      </CustomTabPanel>
+
+          {/* vendas */}
+      <CustomTabPanel value={tabValue} index={3}>
+        <SalesListPage leadId={leadId} />
+      </CustomTabPanel>
+
+          {/* docs a ser retirado futuramente e integrado à page de projetos */}
+      <CustomTabPanel value={tabValue} index={4}>
+        <LeadDocumentPage leadId={leadId} />
+      </CustomTabPanel>
+
+          {/* contratos */}
+      <CustomTabPanel value={tabValue} index={5}></CustomTabPanel>
+
+          {/* agendamentos */}
+      <CustomTabPanel value={tabValue} index={6}>   
+        <LeadSchedulePage leadId={leadId} />
+      </CustomTabPanel>
+
+    </Box>
+  );
+}
+
+LeadsViewSale.propTypes = {
+  leadId: PropTypes.string.isRequired,
+};
 
 export default LeadsViewSale;
