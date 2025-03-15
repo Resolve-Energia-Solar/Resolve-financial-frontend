0edee70e82121a3ee7b49d324bb35d06c757f35c
manu <manuelatourinhodeoliveira@gmail.com>
2025-03-15 00:32:36 -0300

    corrected opacity in typography

--- src/app/components/kanban/Leads/components/ExpandableTableComponent.jsx
+++ src/app/components/kanban/Leads/components/ExpandableTableComponent.jsx
@@ -50,21 +50,21 @@ const ExpandableListComponent = ({
                 <Grid item xs={1} sx={{ justifyContent: "flex-start" }}>
                     <DomainIcon sx={{ verticalAlign: 'middle', color: "#7E8388" }}/>
                 </Grid>
-                <Grid item xs={8} sx={{ justifyContent: "flex-start", display: "flex", flexDirection: "column" }}>
+                <Grid item xs={5} sx={{ justifyContent: "flex-start", display: "flex", flexDirection: "column" }}>
                     <Typography sx={{ fontWeight: 700, fontSize: "14px" }}>Venda</Typography>
-                    <Typography sx={{ fontWeight: 500, fontSize: "16px", color: "#303030" }}>{sale.name}</Typography>
+                    <Typography sx={{ fontWeight: 500, fontSize: "16px", color: "rgba(48, 48, 48, 0.5)" }}>{sale.name}</Typography>
                 </Grid>
-                <Grid item xs={3} sx={{ display: "flex", flexDirection: "row"}}>
-                    <Grid item xs={3}>
+                <Grid item xs={7} sx={{ display: "flex", flexDirection: "row", justifyContent: "flex-end"}}>
+                    <Grid item xs={4} sx={{ alignItems: "center"}}>
                         <Chip label={sale.status} color="primary" variant="outlined" />
                     </Grid>
-                    <Grid item>
-                        <Typography sx={{ fontWeight: 700, fontSize: "14px" }}>Venda</Typography>
-                        <Typography sx={{ fontWeight: 500, fontSize: "16px", color: "#303030" }}>{sale.name}</Typography>
+                    <Grid item xs={4}>
+                        <Typography sx={{ fontWeight: 700, fontSize: "14px" }}>Data de Upload</Typography>
+                        <Typography sx={{ fontWeight: 500, fontSize: "16px", color: "rgba(48, 48, 48, 0.5)" }}>24/10/2019</Typography>
                     </Grid>
-                    <Grid item>
-                        <Typography sx={{ fontWeight: 700, fontSize: "14px" }}>Venda</Typography>
-                        <Typography sx={{ fontWeight: 500, fontSize: "16px", color: "#303030" }}>{sale.name}</Typography>
+                    <Grid item xs={4}>
+                        <Typography sx={{ fontWeight: 700, fontSize: "14px" }}>Código do Contrato</Typography>
+                        <Typography sx={{ fontWeight: 500, fontSize: "16px", color: "rgba(48, 48, 48, 0.5)" }}>#3015893</Typography>
                     </Grid>
                 </Grid>
               
