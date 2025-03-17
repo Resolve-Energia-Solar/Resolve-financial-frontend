37df3c79a94e4a4996359555aca34c01a1ab43c2
manu <manuelatourinhodeoliveira@gmail.com>
2025-03-17 10:48:54 -0300

    commented out header

--- src/app/components/kanban/Leads/Leads-sales/Projects/ClientDataPage.jsx
+++ src/app/components/kanban/Leads/Leads-sales/Projects/ClientDataPage.jsx
@@ -172,7 +172,7 @@ function ClientDataPage({ leadId = null }) {
                                     justifyContent={'space-between'}
                                     sx={{ minHeight: 300, p: 3 }}
                                 >
-                                    <Grid
+                                    {/* <Grid
                                         item
                                         xs={12}
                                         sx={{
@@ -232,7 +232,7 @@ function ClientDataPage({ leadId = null }) {
                                             />
                                         </Grid>
 
-                                    </Grid>
+                                    </Grid> */}
 
                                     <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
                                         <Grid item xs={4}>
