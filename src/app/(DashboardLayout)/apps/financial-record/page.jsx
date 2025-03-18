bb040330dfd115c9bf122b99572ef2db062c78f0
Leonardo Costa <leonardoabreu1212@gmail.com>
2025-03-18 11:21:58 -0300

    fix: organize financialRecordFilterConfig

--- src/app/(DashboardLayout)/apps/financial-record/page.jsx
+++ src/app/(DashboardLayout)/apps/financial-record/page.jsx
@@ -196,14 +196,7 @@ const financialRecordList = () => {
     };
 
     const financialRecordFilterConfig = [
-        {
-            key: 'bug',
-            label: 'Com Erro?',
-            type: 'checkbox',
-            inputType: 'checkbox',
-            trueLabel: 'Com erro',
-            falseLabel: 'Sem erro',
-        },
+        // **Informações Gerais**
         {
             key: 'client_supplier_code',
             label: 'Beneficiário (Omie)',
@@ -213,22 +206,6 @@ const financialRecordList = () => {
                 value && typeof value === 'object' ? value.codigo_cliente : value,
         },
         {
-            key: 'project',
-            label: 'Projeto',
-            type: 'custom',
-            customComponent: AutoCompleteProject,
-            customTransform: (value) =>
-                value && typeof value === 'object' ? value.id : value,
-        },
-        {
-            key: 'department_code__icontains',
-            label: 'Departamento Causador (Omie)',
-            type: 'custom',
-            customComponent: AutoCompleteDepartment,
-            customTransform: (value) =>
-                value && typeof value === 'object' ? value.codigo : value,
-        },
-        {
             key: 'category_code__icontains',
             label: 'Categoria (Omie)',
             type: 'custom',
@@ -236,9 +213,11 @@ const financialRecordList = () => {
             customTransform: (value) =>
                 value && typeof value === 'object' ? value.codigo : value,
         },
+
+        // **Protocolos e Fatura**
         {
             key: "protocol__in",
-            label: "Lista de Protocolos",
+            label: "Protocolo(s)",
             type: "async-multiselect",
             endpoint: "/api/financial-records/",
             queryParam: "protocol__icontains",
@@ -248,66 +227,67 @@ const financialRecordList = () => {
                 value: financialRecord.protocol
             }))
         },
-        { key: "status__in", label: "Status (Lista)", type: "multiselect", options: [] },
+        { key: "invoice_number__icontains", label: "Número da Fatura (Contém)", type: "text" },
+
+        // **Status e Situação**
+        { key: 'bug', label: 'Com Erro?', type: 'checkbox', inputType: 'checkbox', trueLabel: 'Com erro', falseLabel: 'Sem erro' },
+        {
+            key: "status__in", label: "Status", type: "multiselect", options: [
+                { label: "Solicitada", value: "S" },
+                { label: "Em Andamento", value: "E" },
+                { label: "Paga", value: "P" },
+                { label: "Cancelada", value: "C" }
+            ]
+        },
+        {
+            key: "payment_status__in", label: "Status de Pagamento", type: "multiselect", options: [
+                { label: "Paga", value: "PG" },
+                { label: "Pendente", value: "P" },
+                { label: "Cancelada", value: "C" }
+            ]
+        },
+        {
+            key: "responsible_status__in", label: "Status do Responsável", type: "multiselect", options: [
+                { label: "Aprovada", value: "A" },
+                { label: "Pendente", value: "P" },
+                { label: "Reprovada", value: "R" }
+            ]
+        },
+
+        // **Datas**
+        { key: "service_date__range", label: "Serviço realizado entre", type: "range", inputType: "date" },
+        { key: "due_date__range", label: "Vencimento entre", type: "range", inputType: "date" },
+        { key: "created_at__range", label: "Criado entre", type: "range", inputType: "date" },
+        { key: "responsible_response_date__range", label: "Aprovada entre", type: "range", inputType: "date" },
+        { key: "paid_at__range", label: "Paga entre", type: "range", inputType: "date" },
+
+        // **Valores**
         {
             key: "value_range",
             label: "Valor",
             type: "number-range",
             subkeys: { min: "value__gte", max: "value__lte" }
         },
-        { key: "due_date__range", label: "Data de Vencimento (Entre)", type: "range", inputType: "date" },
-        { key: "service_date__range", label: "Data de Serviço (Entre)", type: "range", inputType: "date" },
+
+        // **Outros**
+        { key: "notes__icontains", label: "Observação (Contém)", type: "text" },
+        { key: "requester", label: "Solicitante", type: "async-autocomplete", endpoint: "/api/users/", queryParam: "complete_name__icontains", extraParams: { fields: "complete_name,id" }, mapResponse: (data) => data.results.map(user => ({ label: user.complete_name, value: user.id })) },
+        { key: "responsible", label: "Gestor", type: "async-autocomplete", endpoint: "/api/users/", queryParam: "complete_name__icontains", extraParams: { fields: "complete_name,id" }, mapResponse: (data) => data.results.map(user => ({ label: user.complete_name, value: user.id })) },
+        { key: "responsible_notes__icontains", label: "Observação do Gestor (Contém)", type: "text" },
+        { key: "project", label: "Projeto (Cliente)", type: "custom", customComponent: AutoCompleteProject, customTransform: (value) => value && typeof value === 'object' ? value.id : value },
+
+        // **Departamentos**
         { key: "requesting_department", label: "Departamento Solicitante", type: "text" },
-        { key: "department_code__icontains", label: "Código do Departamento (Contém)", type: "text" },
-        { key: "department_code__in", label: "Código do Departamento (Lista)", type: "multiselect", options: [] },
-        { key: "category_code__icontains", label: "Código da Categoria (Contém)", type: "text" },
-        { key: "category_code__in", label: "Código da Categoria (Lista)", type: "multiselect", options: [] },
-        { key: "invoice_number__icontains", label: "Número da Fatura (Contém)", type: "text" },
-        { key: "invoice_number__in", label: "Número da Fatura (Lista)", type: "multiselect", options: [] },
-        { key: "notes__icontains", label: "Notas (Contém)", type: "text" },
-        { key: "notes__in", label: "Notas (Lista)", type: "multiselect", options: [] },
         {
-            key: "requester",
-            label: "Solicitante",
-            type: "async-autocomplete",
-            endpoint: "/api/users/",
-            queryParam: "complete_name__icontains",
-            extraParams: { fields: "complete_name,id" },
-            mapResponse: (data) => data.results.map(user => ({
-                label: user.complete_name,
-                value: user.id
-            }))
-        },
-        { key: "created_at__range", label: "Criado em (Entre)", type: "range", inputType: "date" },
-        {
-            key: "responsible",
-            label: "Responsável",
-            type: "async-autocomplete",
-            endpoint: "/api/users/",
-            queryParam: "complete_name__icontains",
-            extraParams: { fields: "complete_name,id" },
-            mapResponse: (data) => data.results.map(user => ({
-                label: user.complete_name,
-                value: user.id
-            }))
-        }, {
-            key: "responsible_status__in", label: "Status do Responsável (Lista)", type: "multiselect", options: [
-                { label: "Aprovada", value: "A" },
-                { label: "Pendente", value: "P" },
-                { label: "Reprovada", value: "R" }
-            ]
-        },
-        { key: "responsible_response_date__range", label: "Data de Resposta (Entre)", type: "range", inputType: "date" },
-        { key: "responsible_notes__icontains", label: "Notas do Responsável (Contém)", type: "text" },
-        { key: "responsible_notes__in", label: "Notas do Responsável (Lista)", type: "multiselect", options: [] },
-        {
-            key: "payment_status__in", label: "Status de Pagamento (Lista)", type: "multiselect", options: [
-                { label: "Paga", value: "PG" },
-                { label: "Pendente", value: "P" },
-                { label: "Cancelada", value: "C" }
-            ]
-        },
-        { key: "paid_at__range", label: "Pago em (Entre)", type: "range", inputType: "date" },
+            key: 'department_code__icontains',
+            label: 'Departamento Causador (Omie)',
+            type: 'custom',
+            customComponent: AutoCompleteDepartment,
+            customTransform: (value) =>
+                value && typeof value === 'object'
+                    ? { codigo: value.codigo, descricao: value.descricao }
+                    : value,
+        }
     ];
 
     // Função para lidar com o clique nos KPIs e aplicar filtros,
@@ -354,7 +334,7 @@ const financialRecordList = () => {
             title: "Valor Total",
             value: totalAmount,
             count: totalRequests,
-            isCurrency: true,   // <--- Verdadeiro, pois queremos R$
+            isCurrency: true,
             onClick: () => handleKPIClick("totalAmount"),
         },
         {
