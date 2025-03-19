5678b1e8bb7a9d5b3a944bada90c190a158b52e0
Thiago Menezes <thimenezes19@gmail.com>
2025-03-19 15:51:18 -0300

    feat: add schedule folder

---
+++ src/app/(DashboardLayout)/apps/commercial/schedules/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CommercialSchedulesListing = () => {
+    return (
+        <div>
+        <h1>Commercial Schedules Listing</h1>
+        </div>
+    )
+}
+
+export default CommercialSchedulesListing
