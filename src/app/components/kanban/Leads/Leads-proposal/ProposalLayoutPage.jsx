892d3dc0979f895c568531cc68fb436bb27795af
manu <manuelatourinhodeoliveira@gmail.com>
2025-03-19 11:57:04 -0300

    added page to get the url query and pass to the pdf component creator

---
+++ src/app/components/kanban/Leads/Leads-proposal/ProposalLayoutPage.jsx
@@ -0,0 +1,21 @@
+import { useRouter } from "next/router"
+import ProposalLayout from "../components/ProposalLayout";
+
+const ProposalLayoutPage = () => {
+    const router = useRouter();
+    const { query } = router;
+
+    const formData = {
+        amount: query.amount || '',
+        seller_id: query.seller_id || '',
+        proposal_validity: query.proposal_validity || '',
+        payment: query.payment || '',
+        description: query.description || '',
+    };
+
+    return (
+        <>
+            <ProposalLayout formData={formData} />
+        </>
+    );
+}
