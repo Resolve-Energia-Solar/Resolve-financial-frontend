cde8cd03d7dadfc675cfe199ec5e2a4d98120406
manu <manuelatourinhodeoliveira@gmail.com>
2025-03-22 02:21:26 -0300

    closing dialog has strange behavior. the contet dispear but not completly

--- src/app/components/kanban/Leads/components/ProposalLayout.jsx
+++ src/app/components/kanban/Leads/components/ProposalLayout.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Grid, Typography } from "@mui/material";
+import { Box, Button, Dialog, Grid, Typography } from "@mui/material";
 import html2canvas from "html2canvas";
 import jsPDF from "jspdf";
 import Image from "next/image";
@@ -7,7 +7,11 @@ import React, { useState } from "react";
 
 export default function ProposalLayout({ formData }) {
 
-  // const [openEditProposal, setOpenEditProposal] = useState();
+  const [openDialog, setOpenDialog] = useState(true); 
+
+  const handleCloseDialog = () => {
+    setOpenDialog(false);
+  };
 
   const printRef = React.useRef(null);
 
@@ -39,119 +43,104 @@ export default function ProposalLayout({ formData }) {
   }
 
   return (
-    <Grid container>
-      <Grid container xs={12} sx={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "flex-start", mb: 2 }}>
-        <Grid item xs={2.3}>
-          <Box className="mt-6 flex justify-center">
-            <Button
-              variant="contained"
-              onClick={handleDownloadPdf}
-              className="flex items-center bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition duration-300"
-            >
-              Baixar PDF
-            </Button>
+    <Dialog open={openDialog} onClose={handleCloseDialog}>
+      <Grid container>
+        <Grid container xs={12} sx={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "flex-start", mb: 2 }}>
+          <Grid item xs={2.3}>
+            <Box className="mt-6 flex justify-center">
+              <Button
+                variant="contained"
+                onClick={handleDownloadPdf}
+                className="flex items-center bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition duration-300"
+              >
+                Baixar PDF
+              </Button>
 
-          </Box>
-        </Grid>
-        <Grid item xs={3}>
-          <Box>
-            <Button
-              variant="contained"
-              onClick={handleDownloadPdf}
-              className="flex items-center bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition duration-300"
-            >
-              Alterar proposta
-            </Button>
-
-          </Box>
-        </Grid>
-
-      </Grid>
-      <Grid container ref={printRef} sx={{ position: "relative" }}>
-
-        <Grid item xs={12} sx={{ position: "relative" }}>
-          <Box sx={{ position: "relative", width: "100%"}}>
-            <img
-              src="/images/proposal/proposal_cover_background.png"
-              alt="header yellow bar"
-              style={{ width: "100%", height: "auto" }}
-            />
-            <Box
-              sx={{
-                position: "absolute",
-                top: "55%", 
-                left: "7%",
-              }}
-            >
-              <Image
-                src="/images/logos/resolve-logo.png"
-                alt="logo"
-                height={27}
-                width={85}
-                priority
-              />
             </Box>
-            <Box
-              sx={{
-                position: "absolute",
-                top: "65%", 
-                left: "7%",
-                color: "white", 
-                fontSize: "24px",
-                fontWeight: "bold",
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "flex-start", 
-                
-              }}
-            >
-              <Typography sx={{fontSize: "47px", fontWeight: "400", color: "#000000", mb: 3}}>
-                PROPOSTA
-              </Typography>
-              <Typography sx={{fontSize: "47px", fontWeight: "700", color: "#000000"}}>
-                COMERCIAL
-              </Typography>
-            </Box>
-          </Box>
-        </Grid>
-
-        <Grid item xs={12}>
-          <Box sx={{ position: "relative" }}>
-            <img
-              src="/images/proposal/bottom_border.png"
-              alt="header yellow bar"
-              style={{
-                position: "absolute",
-                bottom: 0, 
-                left: 0,
-                width: "100%",
-                height: "auto",
-                // zIndex: -1,
-              }}
-            />
-          </Box>
-        </Grid>
-
-        {/* <Grid item xs={12}>
+          </Grid>
+          <Grid item xs={3}>
             <Box>
+              <Button
+                variant="contained"
+                onClick={handleCloseDialog}
+                className="flex items-center bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition duration-300"
+              >
+                Alterar proposta
+              </Button>
+
+            </Box>
+          </Grid>
+
+        </Grid>
+        <Grid container ref={printRef} sx={{ position: "relative" }}>
+
+          <Grid item xs={12} sx={{ position: "relative" }}>
+            <Box sx={{ position: "relative", width: "100%" }}>
               <img
                 src="/images/proposal/proposal_cover_background.png"
                 alt="header yellow bar"
-                sx={{ width: "100%", height: "auto" }}
+                style={{ width: "100%", height: "auto" }}
               />
+              <Box
+                sx={{
+                  position: "absolute",
+                  top: "55%",
+                  left: "7%",
+                }}
+              >
+                <Image
+                  src="/images/logos/resolve-logo.png"
+                  alt="logo"
+                  height={27}
+                  width={85}
+                  priority
+                />
+              </Box>
+              <Box
+                sx={{
+                  position: "absolute",
+                  top: "65%",
+                  left: "7%",
+                  color: "white",
+                  fontSize: "24px",
+                  fontWeight: "bold",
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "flex-start",
+
+                }}
+              >
+                <Typography sx={{ fontSize: "47px", fontWeight: "400", color: "#000000", mb: 3 }}>
+                  PROPOSTA
+                </Typography>
+                <Typography sx={{ fontSize: "47px", fontWeight: "700", color: "#000000" }}>
+                  COMERCIAL
+                </Typography>
+              </Box>
             </Box>
           </Grid>
 
           <Grid item xs={12}>
-            <Box>
+            <Box sx={{ position: "relative" }}>
               <img
                 src="/images/proposal/bottom_border.png"
                 alt="header yellow bar"
-                sx={{ width: "100%", height: "auto" }}
+                style={{
+                  position: "absolute",
+                  bottom: 0,
+                  left: 0,
+                  width: "100%",
+                  height: "auto",
+                  // zIndex: -1,
+                }}
               />
             </Box>
-          </Grid>*/}
+          </Grid>
+        </Grid>
+        
       </Grid>
-    </Grid>
+    </Dialog>
+
+
   );
 }
