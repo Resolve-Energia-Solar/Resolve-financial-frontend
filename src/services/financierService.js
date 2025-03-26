92a3740b480f5f2a73b3294d51724e0c57a0c1cb
Yasmim de Albuquerque <yasmimcard990@gmail.com>
2025-03-26 15:50:58 -0300

    refactor: refactoring finacierService

--- src/services/financierService.js
+++ src/services/financierService.js
@@ -24,7 +24,7 @@ const financierService = {
   },
   create: async (data) => {
     try {
-      const response = apiClient.post(`${DEFAULT_ROUTER}/`, data);
+      const response = apiClient.post(`${DEFAULT_ROUTER}`, data);
       return response.data;
     } catch (error) {
       console.error('Erro ao criar financeira:', error);
