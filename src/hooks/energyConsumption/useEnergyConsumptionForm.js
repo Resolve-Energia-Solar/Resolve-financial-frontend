119d9cd8db65a8a62074426080cd42b04e0e60c5
manu <manuelatourinhodeoliveira@gmail.com>
2025-04-01 17:59:54 -0300

    handle change

--- src/hooks/energyConsumption/useEnergyConsumptionForm.js
+++ src/hooks/energyConsumption/useEnergyConsumptionForm.js
@@ -1,12 +1,50 @@
-import { useState } from "react";
+import ProposalService from "@/services/proposalService";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux"
 
 const useEnergyConsumptionForm = (initialData, id) => {
     const user = useSelector((state) => state.user?.user);
 
-    // const [formData, setFormData] = useState({
-    //     lead: 
-    // });
+    const [formData, setFormData] = useState({
+        project: null,
+        medium_consumption: "",
+        appliances_kwh_sum: "",
+
+    });
+
+    const [formErrors, setFormErrors] = useState({});
+    const [success, setSuccess] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        if (initialData) {
+            setFormData({
+                project: initialData.project?.id || null,
+                medium_consumption: initialData.medium_consumption || null,
+                appliances_kwh_sum: initialData.appliances_kwh_sum || null,
+            })
+        }
+    }, [initialData, project?.id]);
+
+    const handleChange = (field, value) => {
+        setFormData((prev) => ({ ...prev, [field]: value }));
+    };
+
+    const handleSave = async () => {
+        setLoading(true);
+        const dataToSend = {
+            project: formData.project,
+            medium_consumption: formData.medium_consumption,
+            appliances_kwh_sum: formData.appliances_kwh_sum,
+        };
+
+        // try {
+        //     if (id) {
+        //         await proposal
+        //     }
+        // }
+    }
+
 };
 
 export default useEnergyConsumptionForm;
