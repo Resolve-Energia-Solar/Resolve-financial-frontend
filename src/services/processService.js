8e319edb29d88e8fd68cf3afa98d4eae50697258
Leonardo Costa <leonardoabreu1212@gmail.com>
2025-04-04 20:51:21 -0300

    feat: add processService

---
+++ src/services/processService.js
@@ -0,0 +1,26 @@
+import apiClient from './apiClient';
+
+const DEFAULT_ROUTER = '/api/process';
+
+const processService = {
+  find: async (id, params = {}) => {
+    try {
+      const response = await apiClient.get(`${DEFAULT_ROUTER}/${id}/`, { params });
+      return response.data;
+    } catch (error) {
+      console.error('Erro ao buscar dados:', error);
+      throw error;
+    }
+  },
+  update: async (id, body) => {
+    try {
+      const response = await apiClient.patch(`${DEFAULT_ROUTER}/${id}/`, body);
+      return response.data;
+    } catch (error) {
+      console.error('Erro ao buscar dados:', error);
+      throw error;
+    }
+  }
+};
+
+export default processService;
