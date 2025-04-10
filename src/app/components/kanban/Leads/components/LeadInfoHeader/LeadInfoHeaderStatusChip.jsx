baebdd73998f1818179056cc16b5a6907fb79b96
manu <manuelatourinhodeoliveira@gmail.com>
2025-04-10 10:20:46 -0300

    lead chip status separated

---
+++ src/app/components/kanban/Leads/components/LeadInfoHeader/LeadInfoHeaderStatusChip.jsx
@@ -0,0 +1,33 @@
+import { Chip, Grid, Typography } from "@mui/material";
+import { useContext } from "react";
+import { LeadModalTabContext } from "../../context/LeadModalTabContext";
+import { useTheme } from "@emotion/react";
+
+export function LeadInfoHeaderStatusChip({ leadId, tabValue }) {
+    const { lead } = useContext(LeadModalTabContext);
+    const theme = useTheme();
+    
+    return (
+        <Grid
+              item
+              sx={{
+                display: 'flex',
+                flexDirection: 'column ',
+                alignItems: 'flex-start',
+              }}
+            >
+              <Typography variant="caption" sx={{ color: 'gray', mb: 0.5 }}>
+                Status
+              </Typography>
+              <Chip
+                label={lead?.column?.name}
+                variant="outlined"
+                sx={{
+                  color: 'gray',
+                  borderColor: `${lead?.column?.color}`,
+                  px: 1,
+                }}
+              />
+        </Grid>
+    );
+}
