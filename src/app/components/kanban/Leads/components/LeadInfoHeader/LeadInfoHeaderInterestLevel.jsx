493baf38fe237b0cbe078db26d9d756821750d79
manu <manuelatourinhodeoliveira@gmail.com>
2025-04-10 10:17:37 -0300

    lead interest and naming separated

---
+++ src/app/components/kanban/Leads/components/LeadInfoHeader/LeadInfoHeaderInterestLevel.jsx
@@ -0,0 +1,46 @@
+import { Grid, useTheme, Typography, Rating } from "@mui/material";
+import { useContext } from "react";
+import { LeadModalTabContext } from "../../context/LeadModalTabContext";
+import { WbSunny } from "@mui/icons-material";
+
+export function LeadInfoHeaderInterestLevel({ leadId, tabValue }) {
+
+    const { lead } = useContext(LeadModalTabContext);
+    const theme = useTheme();
+
+    return (
+        <>
+            <Grid
+                item
+                xs={4}
+                sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}
+            >
+                <Grid
+                    item
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column ',
+                        alignItems: 'flex-start',
+                        mr: 5,
+                    }}
+                >
+                    <Typography variant="caption" sx={{ color: 'gray', mb: 0.5 }}>
+                        Nível de interesse
+                    </Typography>
+                    <Rating
+                        value={lead?.qualification}
+                        max={5}
+                        readOnly
+                        size="normal"
+                        sx={{ gap: 0.5 }}
+                        icon={<WbSunny fontSize="inherit" sx={{ color: theme.palette.primary.main }} />}
+                        emptyIcon={
+                            <WbSunny fontSize="inherit" sx={{ color: theme.palette.action.disabled }} />
+                        }
+                    />
+                </Grid>
+
+            </Grid>
+        </>
+    );
+}
