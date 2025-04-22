c828078cac546a46d09ebb54c4978fb1f2f6ab04
manu <manuelatourinhodeoliveira@gmail.com>
2025-04-22 13:15:53 -0300

    table root with context approach

--- src/app/components/Table/TableRoot.jsx
+++ src/app/components/Table/TableRoot.jsx
@@ -1,9 +1,24 @@
-import { Grid } from "@mui/material";
+import { TableContainer, Table as MuiTable, Grid } from "@mui/material";
+import React, { createContext } from "react";
 
-export function TableRoot({ children }) {
+export const TableCtxt = createContext({});
+
+export function TableRoot({ data, columns, totalRows, page, rowsPerPage, onPageChange, onRowsPerPageChange, children }) {
+    // return (
+    //     <Grid container xs={12}>
+    //         {children}
+    //     </Grid>
+    // );
+
     return (
-        <Grid container xs={12}>
-            {children}
-        </Grid>
+        <TableCtxt.Provider value={{ data, columns, totalRows, page, rowsPerPage, onPageChange, onRowsPerPageChange }}>
+            <Grid container xs={12}>
+                <TableContainer sx={{ borderRadius: 2 , overflow: 'hidden', backgroundColor: '#F9FAFB' }}>
+                    <MuiTable sx={{ borderCollapse: 'separate', borderSpacing: '0px 8px' }}>
+                        {children}
+                    </MuiTable>
+                </TableContainer>
+            </Grid>
+        </TableCtxt.Provider>
     );
 }
