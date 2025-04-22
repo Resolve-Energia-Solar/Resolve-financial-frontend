93576b1fb794f944b399a7a4b8843c9a9e021e22
manu <manuelatourinhodeoliveira@gmail.com>
2025-04-22 17:35:57 -0300

    added box shadow and broder style

--- src/app/components/Table/TableRoot.jsx
+++ src/app/components/Table/TableRoot.jsx
@@ -14,7 +14,7 @@ export function TableRoot({ data, columns, totalRows, page, rowsPerPage, onPageC
         <TableCtxt.Provider value={{ 
             data, columns, totalRows, page, rowsPerPage, onPageChange, onRowsPerPageChange 
         }}>
-            <Grid container xs={12} sx={{ padding: 3, borderRadius: 2 }}>
+            <Grid container xs={12} sx={{ border: '1px solid rgba(126, 131, 136, 0.17)', boxShadow: 2 , px: 3, borderRadius: 2, boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.1)' }}>
                 <TableContainer>
                     <MuiTable sx={{ borderCollapse: 'separate', borderSpacing: '0px 8px' }}>
                         {children}
