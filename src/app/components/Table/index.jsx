8dd1ecc475db8dc588bb8905acff30d82ddecd27
manu <manuelatourinhodeoliveira@gmail.com>
2025-04-22 14:18:34 -0300

    index updated with other subcomponents

--- src/app/components/Table/index.jsx
+++ src/app/components/Table/index.jsx
@@ -1,13 +1,21 @@
 import { TableBody } from "./TableBody";
+import { TableCell } from "./TableCell";
+import { TableEditAction } from "./TableEditAction";
+import { TableHead } from "./TableHead";
 import { TableHeader } from "./TableHeader";
+import { TablePagination } from "./TablePagination";
 import { TableRoot } from "./TableRoot";
+import { TableSwitchAction } from "./TableSwitchAction";
+import { TableViewAction } from "./TableViewAction";
 
 export const Table = {
     Root: TableRoot,
     Header: TableHeader,
+    Head: TableHead,
     Body: TableBody,
-    // Row: TableRow,
-    // Cell: TableCell,
-    // Actions: TableActions,
-    // Pagination: TablePagination,
+    Cell: TableCell,
+    EditAction: TableEditAction,
+    ViewAction: TableViewAction,
+    SwitchAction: TableSwitchAction,
+    Pagination: TablePagination,
 }
