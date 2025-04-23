0e074fc3df1b9e9fde2efc1f2a5032c78ae9cbce
manu <manuelatourinhodeoliveira@gmail.com>
2025-04-23 13:53:13 -0300

    title color to 303030

--- src/app/components/TableHeader/TableHeaderTitle.jsx
+++ src/app/components/TableHeader/TableHeaderTitle.jsx
@@ -3,7 +3,7 @@ import { Grid, Typography } from "@mui/material";
 export default function TableHeaderTitle({ title, totalItems, objNameNumberReference }) {
     return (
         <Grid item gridArea={"title"} sx={{ display: 'flex', flexDirection: "row", justifyContent: 'flex-end' }}>
-            <Typography sx={{ fontSize: '16px', color: "#092C4C" }}>
+            <Typography sx={{ fontSize: '16px', color: "#303030" }}>
                 <span style={{ fontWeight: 'bold' }}>{title}: </span> {totalItems} {objNameNumberReference}
             </Typography>
         </Grid>
