b72419e022e5632038bb76443359a2b6a4994f4d
manu <manuelatourinhodeoliveira@gmail.com>
2025-04-23 13:15:21 -0300

    generic button

---
+++ src/app/components/TableHeader/TableHeaderButton.jsx
@@ -0,0 +1,28 @@
+import { Button, Grid } from "@mui/material";
+import { Add } from "@mui/icons-material";
+
+export default function TableHeaderButton({ buttonLabel, onButtonClick, sx }) {
+    return (
+        <Grid item xs={1} sx={{ display: 'flex', flexDirection: "row", justifyContent: 'flex-end' }}>
+            <Button
+                startIcon={<Add />}
+                onClick={onButtonClick}
+                sx={{
+                    width: 74,
+                    height: 28,
+                    fontSize: '0.75rem',
+                    p: '4px 8px',
+                    minWidth: 'unset',
+                    borderRadius: '4px',
+                    color: '#000',
+                    backgroundColor: theme.palette.primary.main,
+                    '&:hover': { backgroundColor: theme.palette.primary.light, color: '#000' },
+                    ... sx,
+                }}
+            >
+                {buttonLabel}
+
+            </Button>
+        </Grid>
+    );
+}
