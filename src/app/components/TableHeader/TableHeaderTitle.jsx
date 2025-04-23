7e44c52dc4b1714ea5c41927a376e020af39e26e
manu <manuelatourinhodeoliveira@gmail.com>
2025-04-23 13:56:16 -0300

    title color to custom changeable

--- src/app/components/TableHeader/TableHeaderTitle.jsx
+++ src/app/components/TableHeader/TableHeaderTitle.jsx
@@ -1,9 +1,10 @@
-import { Grid, Typography } from "@mui/material";
+import { Grid, Typography, useTheme } from "@mui/material";
 
-export default function TableHeaderTitle({ title, totalItems, objNameNumberReference }) {
+export default function TableHeaderTtle({ title, totalItems, objNameNumberReference }) {
+    const theme = useTheme();
     return (
         <Grid item gridArea={"title"} sx={{ display: 'flex', flexDirection: "row", justifyContent: 'flex-end' }}>
-            <Typography sx={{ fontSize: '16px', color: "#303030" }}>
+            <Typography sx={{ fontSize: '16px', color: theme.palette.primary.title }}>
                 <span style={{ fontWeight: 'bold' }}>{title}: </span> {totalItems} {objNameNumberReference}
             </Typography>
         </Grid>
