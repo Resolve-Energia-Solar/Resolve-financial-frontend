2f26c240c2b0cd5a7bfc833e743b4ee75aa3cb0f
manu <manuelatourinhodeoliveira@gmail.com>
2025-04-23 13:11:14 -0300

    grid style and composition with children

--- src/app/components/TableHeader/TableHeaderRoot.jsx
+++ src/app/components/TableHeader/TableHeaderRoot.jsx
@@ -1,11 +1,10 @@
+import { Grid } from "@mui/material";
+
 export function TableHeaderRoot({ children }) {
+    
     return (
         <Grid container xs={12} sx={{ display: 'flex', flexDirection: "row", justifyContent: 'space-between', width: '100%', alignItems: 'center', p: 2 }}>
-            <Grid item xs={5}>
-                <Typography sx={{ fontSize: '16px', color: "#092C4C" }}>
-                    <span style={{ fontWeight: 'bold' }}>Tabela de Leads</span>
-                </Typography>
-            </Grid>
+            {children}
         </Grid>
     );
 }
