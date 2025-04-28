6b62e06335700e152c5bf7df06a1a32af3076404
manu <manuelatourinhodeoliveira@gmail.com>
2025-04-28 12:04:22 -0300

    text body

--- src/app/components/apps/project/components/ClientCard/ClientCardText.jsx
+++ src/app/components/apps/project/components/ClientCard/ClientCardText.jsx
@@ -1,3 +1,22 @@
-export default function ClientCardText() {
-    
+import { Grid, useTheme, Typography, } from "@mui/material"
+
+export default function ClientCardText({ title, content }) {
+
+    const theme = useTheme();
+
+    return (
+        <Grid item sx={{ gridArea: 'profile' }}>
+            <Grid container size="auto" alignItems="center" spacing={2}>
+                <Grid item size="auto" sx={{ position: 'relative', display: 'inline-block'}}>
+                    <Typography variant="caption" sx={{ color: 'gray' }}>
+                        {title}
+                    </Typography>
+                    <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
+                        {content}
+                    </Typography>
+                </Grid>
+            </Grid>
+        </Grid>
+    );
+
 }
