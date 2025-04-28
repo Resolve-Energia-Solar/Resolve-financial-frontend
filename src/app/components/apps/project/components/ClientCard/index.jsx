69c7eec049a049afe596941343d3dd6b4b97638e
manu <manuelatourinhodeoliveira@gmail.com>
2025-04-28 12:01:02 -0300

    indexes references

--- src/app/components/apps/project/components/ClientCard/index.jsx
+++ src/app/components/apps/project/components/ClientCard/index.jsx
@@ -1,3 +1,10 @@
-export default function ClientCard() {
-    
+import ClientCardChips from "./ClientCardChips";
+import ClientCardRoot from "./ClientCardRoot";
+import ClientCardText from "./ClientCardText";
+
+export const ClientCard = {
+    Root: ClientCardRoot,
+    Text: ClientCardText,
+    Chips: ClientCardChips,
+
 }
