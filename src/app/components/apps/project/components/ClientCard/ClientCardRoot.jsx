f437f78981fb3807972a81ada63d5591075041dd
manu <manuelatourinhodeoliveira@gmail.com>
2025-04-28 14:09:47 -0300

    new border style

--- src/app/components/apps/project/components/ClientCard/ClientCardRoot.jsx
+++ src/app/components/apps/project/components/ClientCard/ClientCardRoot.jsx
@@ -9,8 +9,10 @@ export default function ClientCardRoot({ children }) {
             xs={12}
             sx={{
                 width: '100%',
-                border: '1px solid rgba(224, 224, 224, 0.8)',
-                borderRadius: '20px',
+                border: '1px solid rgba(94, 96, 99, 0.17)',
+                boxShadow: 1,
+                borderRadius: 2,
+                boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.1)',
                 p: '16px',
                 display: 'grid',
                 gridTemplateColumns: 'minmax(100px, auto) 1fr 1fr 1fr',
