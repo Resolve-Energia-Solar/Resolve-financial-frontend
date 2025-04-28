bfd0781059cd97c88e99c4dd7dc7f2afe717aa7e
manu <manuelatourinhodeoliveira@gmail.com>
2025-04-28 11:58:41 -0300

    root style

--- src/app/components/apps/project/components/ClientCard/ClientCardRoot.jsx
+++ src/app/components/apps/project/components/ClientCard/ClientCardRoot.jsx
@@ -1,3 +1,25 @@
-export default function ClientCardRoot() {
+import {
+    Grid,
+} from '@mui/material';
 
+export default function ClientCardRoot({ children }) {
+    return (
+        <Grid
+            container
+            xs={12}
+            sx={{
+                width: '100%',
+                borderBottom: '1px solid #E0E0E0',
+                p: '16px',
+                display: 'grid',
+                gridTemplateColumns: 'minmax(100px, auto) 1fr 1fr 1fr',
+                gridTemplateAreas: `"profile interest status project"`,
+                alignItems: 'center',
+                gap: '1rem'
+            }}
+        >
+            {children}
+        </Grid>
+
+    );
 }
