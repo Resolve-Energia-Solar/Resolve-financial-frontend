c1dbc5f2831315cf9f3e7d885cc59d9694986049
manu <manuelatourinhodeoliveira@gmail.com>
2025-04-30 21:29:52 -0300

    adaptable card width

--- src/app/components/apps/project/Costumer-journey/HorizontalProcessCards.jsx
+++ src/app/components/apps/project/Costumer-journey/HorizontalProcessCards.jsx
@@ -64,7 +64,7 @@ const HorizontalProcessCards = () => {
         ref={scrollRef} 
         display="flex"
         flexDirection="row"
-        gap={2}
+        gap={1}
         overflow="auto"
         padding={2}
         sx={{ backgroundColor: theme.palette.primary.Box }}
@@ -73,15 +73,16 @@ const HorizontalProcessCards = () => {
           <Card
             key={index}
             sx={{
-              width: '250px',
+              minWidth: '170px',
               height: '75px',
+              width: 'auto',
               flexShrink: 0,
               borderRadius: 2,
               py: 2,
               px: 2,
               border: '1px solid rgba(126, 131, 136, 0.17)',
               boxShadow: 4,
-              boxShadow: '0px 8px 12px rgba(0, 0, 0, 0.1)'
+              boxShadow: '8px 8px 12px rgba(0, 0, 0, 0.1)'
 
             }}
             elevation={3}
@@ -96,7 +97,7 @@ const HorizontalProcessCards = () => {
                 textAlign: 'start',
                 // spacing: 10,
               }}>
-              <Typography sx={{ fontWeight: 500, fontSize: '14px', mb: 0.2, mt: 0, opacity: '0.5' }}>{item.step}</Typography>
+              <Typography sx={{ fontWeight: 600, fontSize: '14px', mb: 0.2, mt: 0, opacity: '0.5' }}>{item.step}</Typography>
               <Typography sx={{ fontWeight: 700, fontSize: '20px', mb: 2 }}>{item.total_processes}</Typography>
             </CardContent>
           </Card>
