f5bc7cd43f3f4b76b0694c186d4c508c351b0ee0
Leonardo Costa <leonardoabreu1212@gmail.com>
2025-05-03 12:25:28 -0300

    feat: add purchases table to LogisticsTab

--- src/app/components/apps/project/Costumer-journey/Project-Detail/logistics/LogisticsTab.jsx
+++ src/app/components/apps/project/Costumer-journey/Project-Detail/logistics/LogisticsTab.jsx
@@ -2,28 +2,26 @@ import { useEffect, useState, useCallback } from "react";
 import { useSnackbar } from "notistack";
 import scheduleService from "@/services/scheduleService";
 import TableSkeleton from "../../../../comercial/sale/components/TableSkeleton";
-import EditIcon from "@mui/icons-material/Edit";
-import VisibilityIcon from "@mui/icons-material/Visibility";
 import ScheduleFromProjectForm from "../../../modal/ScheduleFromProjectForm";
 import UserCard from "../../../../users/userCard";
 import { formatDate } from "@/utils/dateUtils";
 import ScheduleOpinionChip from "../../../../inspections/schedule/StatusChip/ScheduleOpinionChip";
 import { Table } from "@/app/components/Table";
-import { useTheme, alpha, Dialog, DialogContent } from "@mui/material";
+import { Dialog, DialogContent } from "@mui/material";
 import { TableHeader } from "@/app/components/TableHeader";
 import categoryService from "@/services/categoryService";
 import DetailsDrawer from "@/app/components/apps/schedule/DetailsDrawer";
+import purchaseService from "@/services/purchaseService";
 
 export default function LogisticsTab({ projectId }) {
     const { enqueueSnackbar } = useSnackbar()
     const [deliveries, setDeliveries] = useState([])
+    const [purchases, setPurchases] = useState([]);
     const [loading, setLoading] = useState(true)
     const [page, setPage] = useState(0)
     const [rowsPerPage, setRowsPerPage] = useState(5)
     const [categoryId, setCategoryId] = useState(null);
     const [selectedDelivery, setSelectedDelivery] = useState(null);
-
-    const theme = useTheme();
 
     const [openDeliveryFormModal, setOpenDeliveryFormModal] = useState(false);
     const [openViewDelivery, setOpenViewDelivery] = useState(false);
@@ -56,7 +54,7 @@ export default function LogisticsTab({ projectId }) {
 
     useEffect(() => {
         fetchDeliveries();
-    }, []);
+    }, [projectId, fetchDeliveries]);
 
     useEffect(() => {
         const fetchCategory = async () => {
@@ -68,7 +66,7 @@ export default function LogisticsTab({ projectId }) {
         fetchCategory();
     }, []);
 
-    const handleFormSuccess = async () => {
+    const handleDeliveryFormSuccess = async () => {
         setOpenDeliveryFormModal(false);
         setSelectedDelivery(null);
         await fetchDeliveries();
@@ -76,11 +74,7 @@ export default function LogisticsTab({ projectId }) {
 
     const products = deliveries.map(i => i.project.products).flat();
 
-    if (loading) {
-        return <TableSkeleton columns={8} rows={4} />;
-    }
-
-    const columns = [
+    const deliveriesColumns = [
         { field: 'service', headerName: 'Serviço', render: r => r.service.name },
         { field: 'address', headerName: 'Endereço', render: r => r.address.complete_address },
         { field: 'schedule_date', headerName: 'Agendada', render: r => new Date(r.schedule_date).toLocaleDateString() },
@@ -89,10 +83,54 @@ export default function LogisticsTab({ projectId }) {
         { field: 'final_service_opinion', headerName: 'Status Logístico', render: r => r.final_service_opinion?.name },
     ]
 
+    const fetchPurchases = useCallback(async () => {
+        if (projectId) {
+            setLoading(true);
+            try {
+                const response = await purchaseService.index(
+                    {
+                        fields: "id,purchase_date,status,delivery_number,project,supplier.complete_name,project.product.description,delivery_type.name",
+                        expand: "supplier,project.product,delivery_type",
+                        project__in: projectId,
+                    }
+                );
+                setPurchases(response.results);
+            } catch (error) {
+                enqueueSnackbar(`Erro ao carregar compras: ${error.message}`, { variant: "error" });
+            } finally {
+                setLoading(false);
+            }
+        } else {
+            setLoading(false);
+        }
+    }, [projectId, enqueueSnackbar]);
+
+    useEffect(() => {
+        fetchPurchases();
+    }, [projectId, fetchPurchases]);
+
+    const handlePurchaseFormSuccess = async () => {
+        // setOpenPurchaseFormModal(false);
+        // setSelectedPurchase(null);
+        await fetchPurchases();
+    };
+
+    if (loading) {
+        return <TableSkeleton columns={8} rows={4} />;
+    }
+
+    const purchasesColumns = [
+        { field: 'purchase_date', headerName: 'Data da Compra', render: r => new Date(r.purchase_date).toLocaleDateString() },
+        { field: 'status', headerName: 'Status', render: r => r.status },
+        { field: 'delivery_number', headerName: 'Número de Entrega', render: r => r.delivery_number },
+        { field: 'supplier', headerName: 'Fornecedor', render: r => r.supplier.complete_name },
+        { field: 'product', headerName: 'Produto', render: r => r.project?.product?.description },
+        { field: 'delivery_type', headerName: 'Tipo de Entrega', render: r => r.delivery_type.name },
+    ]
 
     return (
         <>
-
+            {/* Entregas */}
             <TableHeader.Root>
                 <TableHeader.Title
                     title="Total"
@@ -117,7 +155,7 @@ export default function LogisticsTab({ projectId }) {
                 onRowsPerPageChange={setRowsPerPage}
             >
                 <Table.Head>
-                    {columns.map(c => (
+                    {deliveriesColumns.map(c => (
                         <Table.Cell
                             key={c.field}
                             sx={{ fontWeight: 600, fontSize: '14px' }}
@@ -176,12 +214,80 @@ export default function LogisticsTab({ projectId }) {
                         scheduleId={selectedDelivery || null}
                         categoryId={categoryId}
                         useSupplier={true}
-                        onSave={handleFormSuccess}
+                        onSave={handleDeliveryFormSuccess}
                     />
                 </DialogContent>
             </Dialog>
 
-            <DetailsDrawer dialogMode={true}  scheduleId={selectedDelivery} open={openViewDelivery} onClose={() => setOpenViewDelivery(false)} />
+            <DetailsDrawer dialogMode={true} scheduleId={selectedDelivery} open={openViewDelivery} onClose={() => setOpenViewDelivery(false)} />
+
+            {/* Compras */}
+            <TableHeader.Root>
+                <TableHeader.Title
+                    title="Total"
+                    totalItems={products.length}
+                    objNameNumberReference={products.length === 1 ? "Compra" : "Compras"}
+                />
+                <TableHeader.Button
+                    buttonLabel="Adicionar compra"
+                    onButtonClick={() => { /* setOpenPurchaseFormModal(true) */ }}
+                    sx={{
+                        width: 200,
+                    }}
+                />
+            </TableHeader.Root>
+
+            <Table.Root
+                data={purchases}
+                totalRows={purchases.length}
+                page={page}
+                rowsPerPage={rowsPerPage}
+                onPageChange={setPage}
+                onRowsPerPageChange={setRowsPerPage}
+            >
+                <Table.Head>
+                    {purchasesColumns.map(c => (
+                        <Table.Cell
+                            key={c.field}
+                            sx={{ fontWeight: 600, fontSize: '14px' }}
+                        >
+                            {c.headerName}
+                        </Table.Cell>
+                    ))}
+                    <Table.Cell align="center">Editar</Table.Cell>
+                    <Table.Cell align="center">Ver</Table.Cell>
+                </Table.Head>
+
+                <Table.Body loading={loading}>
+                    <Table.Cell
+                        render={row => row.purchase_date}
+                        sx={{ opacity: 0.7, }}
+                    />
+                    <Table.Cell
+                        render={row => row.status}
+                        sx={{ opacity: 0.7, }}
+                    />
+                    <Table.Cell render={row =>
+                        row.delivery_number || '-'}
+                        sx={{ opacity: 0.7 }}
+                    />
+                    <Table.Cell render={row =>
+                        row.supplier?.complete_name}
+                        sx={{ opacity: 0.7 }}
+                    />
+                    <Table.Cell render={row =>
+                        row.project?.product?.description}
+                        sx={{ opacity: 0.7 }}
+                    />
+                    <Table.Cell render={row =>
+                        row.delivery_type?.name}
+                        sx={{ opacity: 0.7 }}
+                    />
+
+                    <Table.EditAction onClick={row => { /* setSelectedPurchase(row.id); setOpenPurchaseFormModal(true); */ }} />
+                    <Table.ViewAction onClick={row => { /* setSelectedPurchase(row.id); setOpenViewPurchase(true) */ }} />
+                </Table.Body>
+            </Table.Root>
         </>
     );
 }
