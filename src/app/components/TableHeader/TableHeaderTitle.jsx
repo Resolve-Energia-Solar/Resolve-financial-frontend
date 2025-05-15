989fb655fb1e54a1ef6e7a8b6cbf1a8e23095b0a
Leonardo Costa <leonardoabreu1212@gmail.com>
2025-05-15 13:53:25 -0300

    feat: add loading for TableHeaderTitle

--- src/app/components/TableHeader/TableHeaderTitle.jsx
+++ src/app/components/TableHeader/TableHeaderTitle.jsx
@@ -1,13 +1,13 @@
-import { Grid, Typography, useTheme } from "@mui/material";
+import { Grid, Skeleton, Typography, useTheme } from "@mui/material";
 
-export default function TableHeaderTtle({ title, totalItems, objNameNumberReference }) {
+export default function TableHeaderTitle({ title, totalItems, objNameNumberReference, loading }) {
     const theme = useTheme();
     const formattedTotalItems = totalItems?.toLocaleString() ?? 0;
 
     return (
         <Grid item gridArea={"title"} sx={{ display: 'flex', flexDirection: "row", justifyContent: 'flex-end' }}>
             <Typography sx={{ fontSize: '18px', color: theme.palette.primary.title }}>
-                <span style={{ fontWeight: 'bold' }}>{title}: </span> {formattedTotalItems} {objNameNumberReference}
+                <span style={{ fontWeight: 'bold' }}>{title}: </span> {loading ? <Skeleton variant="text" width={40} sx={{ display: 'inline-block' }} /> : formattedTotalItems} {objNameNumberReference}
             </Typography>
         </Grid>
     );
