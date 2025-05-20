4dfae5ad172480472b127159de96c0c88f5959e9
Thiago Menezes <thimenezes19@gmail.com>
2025-05-20 18:31:09 -0300

    fix: fix sicoob

--- src/app/components/apps/project/Costumer-journey/Project-Detail/ProjectDrawer.jsx
+++ src/app/components/apps/project/Costumer-journey/Project-Detail/ProjectDrawer.jsx
@@ -1,6 +1,16 @@
 import { useState, useEffect, useCallback, useMemo } from 'react';
 import PropTypes from 'prop-types';
-import { Box, IconButton, Drawer, Tabs, Tab, Typography, Card, CardContent, Divider } from '@mui/material';
+import {
+  Box,
+  IconButton,
+  Drawer,
+  Tabs,
+  Tab,
+  Typography,
+  Card,
+  CardContent,
+  Divider,
+} from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 import { useSnackbar } from 'notistack';
 import projectService from '@/services/projectService';
@@ -39,7 +49,7 @@ export default function ProjectDetailDrawer({ projectId, open, onClose, refresh 
   const [hasConstructionTab, setHasConstructionTab] = useState(false);
   const user = useSelector((state) => state.user?.user);
   const userPermissions = user?.user_permissions;
-  const canEdit = userPermissions?.includes('resolve_crm.can_manage_journey')
+  const canEdit = userPermissions?.includes('resolve_crm.can_manage_journey');
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
@@ -59,18 +69,19 @@ export default function ProjectDetailDrawer({ projectId, open, onClose, refresh 
     try {
       const [proj, proc] = await Promise.all([
         projectService.find(projectId, {
-          fields: 'id,project_number,sale,customer.complete_name,field_services.service.name,field_services.final_service_opinion.name',
+          fields:
+            'id,project_number,sale,customer.complete_name,field_services.service.name,field_services.final_service_opinion.name',
           expand: 'sale.customer,field_services.service,field_services.final_service_opinion,sale',
-        })
+        }),
       ]);
       setProject(proj);
       setProcessId(proc);
       const constructionKeywords = ['obra', 'sombreamento'];
-      const hasConstruction = proj.field_services?.some(fs => {
+      const hasConstruction = proj.field_services?.some((fs) => {
         if (!fs.service?.name?.toLowerCase().includes('vistoria')) return false;
-        
+
         const opinionName = fs.final_service_opinion?.name?.toLowerCase() || '';
-        return constructionKeywords.some(keyword => opinionName.includes(keyword));
+        return constructionKeywords.some((keyword) => opinionName.includes(keyword));
       });
       setHasConstructionTab(hasConstruction);
     } catch (error) {
@@ -88,54 +99,107 @@ export default function ProjectDetailDrawer({ projectId, open, onClose, refresh 
   const handleTabChange = (e, newVal) => setTab(newVal);
   const drawerWidth = useMemo(() => (processId ? '100vw' : '65vw'), [processId]);
 
-  const tabsConfig = useMemo(() => [
-    { label: 'Cliente', content: <CustomerTab projectId={projectId} viewOnly={!canEdit} /> },
-    { label: 'Contratos', content: <EditSale saleId={project?.sale?.id} /> },
-    { label: 'Vistoria', content: <InspectionsTab projectId={projectId} viewOnly={!canEdit} /> },
-    hasConstructionTab && { label: 'Obras', content: <ConstructionsTab projectId={projectId} viewOnly={!canEdit} /> },
-    { label: 'Financeiro', content: <PaymentCard sale={project?.sale?.id} /> },
-    {
-      label: 'Engenharia', content:
-        <Box sx={{ display: 'flex', flexDirection: 'column', flex: 1, gap: 2 }}>
-          <EditProjectTab projectId={projectId} detail={!canEdit} />
+  const tabsConfig = useMemo(
+    () =>
+      [
+        { label: 'Cliente', content: <CustomerTab projectId={projectId} viewOnly={!canEdit} /> },
+        { label: 'Contratos', content: <EditSale saleId={project?.sale?.id} /> },
+        {
+          label: 'Vistoria',
+          content: <InspectionsTab projectId={projectId} viewOnly={!canEdit} />,
+        },
+        hasConstructionTab && {
+          label: 'Obras',
+          content: <ConstructionsTab projectId={projectId} viewOnly={!canEdit} />,
+        },
+        { label: 'Financeiro', content: <PaymentCard sale={project?.sale?.id} /> },
+        {
+          label: 'Engenharia',
+          content: (
+            <Box sx={{ display: 'flex', flexDirection: 'column', flex: 1, gap: 2 }}>
+              <EditProjectTab projectId={projectId} detail={!canEdit} />
 
-          <Divider sx={{ my: 2 }} />
+              <Divider sx={{ my: 2 }} />
 
-          {canEdit && (
-            <UploadDocument projectId={projectId} />
-          )}
+              {canEdit && <UploadDocument projectId={projectId} />}
 
-          <Divider sx={{ my: 2 }} />
+              <Divider sx={{ my: 2 }} />
 
-          <CheckListRateio projectId={projectId} label="Checklist de Rateio" canEdit={!canEdit} />
-        </Box>
-    },
-    {
-      label: 'Anexos',
-      content: (
-        <>
-          <Card sx={{ my: 3, boxShadow: 7 }}>
-            <CardContent>
-              <Typography variant="h6" sx={{ mb: 1 }}>Anexos do Projeto</Typography>
-              <AttachmentTable hideTitle={true} appLabelDocument="engineering" appLabel="resolve_crm" model="project" objectId={projectId} hideStatus={false} viewOnly={!canEdit} />
-            </CardContent>
-          </Card>
-          <Card sx={{ boxShadow: 3 }}>
-            <CardContent>
-              <Typography variant="h6" sx={{ mb: 1 }}>Anexos do Venda</Typography>
-              <AttachmentTable hideTitle={true} appLabelDocument="contracts" appLabel="resolve_crm" model="sale" objectId={project?.sale?.id} hideStatus={false} viewOnly={!canEdit} />
-            </CardContent>
-          </Card>
-        </>
-      ),
-    },
-    { label: 'Logística', content: <LogisticsTab projectId={projectId} viewOnly={!canEdit} /> },
-    { label: 'Instalação', content: <InstallationsTab projectId={projectId} viewOnly={!canEdit} /> },
-    { label: 'Homologação', content: <RequestList projectId={projectId} enableFilters={false} enableIndicators={false} viewOnly={!canEdit} /> },
-    { label: 'Perdas', content: <LossesTab projectId={projectId} viewOnly={!canEdit} /> },
-    { label: 'Histórico', content: <History objectId={projectId} appLabel="resolve_crm" model="project" /> },
-    { label: 'Comentários', content: <CommentsTab projectId={projectId} userPermissions={userPermissions} /> },
-  ].filter(Boolean), [project, projectId, hasConstructionTab]);
+              <CheckListRateio
+                projectId={projectId}
+                label="Checklist de Rateio"
+                canEdit={!canEdit}
+              />
+            </Box>
+          ),
+        },
+        {
+          label: 'Anexos',
+          content: (
+            <>
+              <Card sx={{ my: 3, boxShadow: 7 }}>
+                <CardContent>
+                  <Typography variant="h6" sx={{ mb: 1 }}>
+                    Anexos do Projeto
+                  </Typography>
+                  <AttachmentTable
+                    hideTitle={true}
+                    appLabelDocument="engineering"
+                    appLabel="resolve_crm"
+                    model="project"
+                    objectId={projectId}
+                    hideStatus={false}
+                    viewOnly={!canEdit}
+                  />
+                </CardContent>
+              </Card>
+              <Card sx={{ boxShadow: 3 }}>
+                <CardContent>
+                  <Typography variant="h6" sx={{ mb: 1 }}>
+                    Anexos do Venda
+                  </Typography>
+                  <AttachmentTable
+                    hideTitle={true}
+                    appLabelDocument="contracts"
+                    appLabel="resolve_crm"
+                    model="sale"
+                    objectId={project?.sale?.id}
+                    hideStatus={false}
+                    viewOnly={!canEdit}
+                  />
+                </CardContent>
+              </Card>
+            </>
+          ),
+        },
+        { label: 'Logística', content: <LogisticsTab projectId={projectId} viewOnly={!canEdit} /> },
+        {
+          label: 'Instalação',
+          content: <InstallationsTab projectId={projectId} viewOnly={!canEdit} />,
+        },
+        {
+          label: 'Homologação',
+          content: (
+            <RequestList
+              projectId={projectId}
+              enableFilters={false}
+              enableIndicators={false}
+              viewOnly={!canEdit}
+            />
+          ),
+        },
+        { label: 'Perdas', content: <LossesTab projectId={projectId} viewOnly={!canEdit} /> },
+        {
+          label: 'Histórico',
+          content: <History objectId={projectId} appLabel="resolve_crm" model="project" />,
+        },
+        {
+          label: 'Comentários',
+          content: <CommentsTab projectId={projectId} userPermissions={userPermissions} />,
+        },
+      ].filter(Boolean),
+    [project, projectId, hasConstructionTab],
+  );
 
   return (
     <Drawer
@@ -156,7 +220,8 @@ export default function ProjectDetailDrawer({ projectId, open, onClose, refresh 
         }}
       >
         <Typography variant="h5">
-          Detalhes do Projeto nº {project?.project_number} - {' '} {project?.sale?.customer?.complete_name}
+          Detalhes do Projeto nº {project?.project_number} -{' '}
+          {project?.sale?.customer?.complete_name}
         </Typography>
         <IconButton onClick={handleClose}>
           <CloseIcon />
@@ -164,9 +229,15 @@ export default function ProjectDetailDrawer({ projectId, open, onClose, refresh 
       </Box>
       <Box sx={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
         <Tabs value={tab} onChange={handleTabChange} variant="scrollable" scrollButtons="auto">
-          {tabsConfig.map((t, i) => <Tab key={i} label={t.label} />)}
+          {tabsConfig.map((t, i) => (
+            <Tab key={i} label={t.label} />
+          ))}
         </Tabs>
-        {tabsConfig.map((t, i) => <TabPanel key={i} value={tab} index={i}>{t.content}</TabPanel>)}
+        {tabsConfig.map((t, i) => (
+          <TabPanel key={i} value={tab} index={i}>
+            {t.content}
+          </TabPanel>
+        ))}
       </Box>
     </Drawer>
   );
