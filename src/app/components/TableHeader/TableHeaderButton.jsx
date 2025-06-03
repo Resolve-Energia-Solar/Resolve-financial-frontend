5f81f0c8f7e0c41f0206e401b7e67eeca67b098b
Leonardo Costa <leonardoabreu1212@gmail.com>
2025-06-03 17:29:17 -0300

    fix: adjust TableHeaderButton colors

--- src/app/components/TableHeader/TableHeaderButton.jsx
+++ src/app/components/TableHeader/TableHeaderButton.jsx
@@ -2,7 +2,7 @@ import { Button, Grid, Typography, useTheme } from "@mui/material";
 
 export default function TableHeaderButton({ buttonLabel, onButtonClick, sx, icon }) {
     const theme = useTheme();
-    
+
     return (
         <Grid item gridArea={"button"} sx={{ display: 'flex', flexDirection: "row", justifyContent: 'flex-end' }}>
             <Button
@@ -12,13 +12,12 @@ export default function TableHeaderButton({ buttonLabel, onButtonClick, sx, icon
                     height: 36,
                     fontSize: '0.75rem',
                     p: '4px 8px',
-                    minWidth: 'unset',
-                    borderRadius: '4px',
-                    color: theme.palette.primary.light,
+                    minWidth: 'max-content',
+                    color: theme.palette.getContrastText(theme.palette.primary.light),
                     borderRadius: 5,
                     backgroundColor: theme.palette.primary.main,
-                    '&:hover': { backgroundColor: theme.palette.primary.light, color: theme.palette.primary.main, border: '1px solid', borderColor: theme.palette.primary.main },
-                    ... sx,
+                    '&:hover': { backgroundColor: theme.palette.primary.dark, color: theme.palette.getContrastText(theme.palette.primary.dark), border: '1px solid', borderColor: theme.palette.getContrastText(theme.palette.background.default) },
+                    ...sx,
                 }}
             >
                 <Typography sx={{ fontWeight: "600" }}> {buttonLabel} </Typography>
