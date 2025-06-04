2a835482a80c9e5fe83c666bcf4bcf620aee7a45
Leonardo Costa <leonardoabreu1212@gmail.com>
2025-06-04 17:25:35 -0300

    refactor: remove alert of missing process for project at SideDrawer

--- src/app/components/shared/SideDrawer.jsx
+++ src/app/components/shared/SideDrawer.jsx
@@ -1,33 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Box, Drawer, IconButton } from '@mui/material';
-import { useSnackbar } from 'notistack';
 import CloseIcon from '@mui/icons-material/Close';
 import ZoomOutMapIcon from '@mui/icons-material/ZoomOutMap';
 import ZoomInMapIcon from '@mui/icons-material/ZoomInMap';
 
 import ParentCard from './ParentCard';
-import processService from '@/services/processService';
 
 export default function SideDrawer({ title, children, open, onClose, anchor = 'right', projectId }) {
-  const { enqueueSnackbar } = useSnackbar();
   const [expandView, setExpand] = useState(false);
 
-  useEffect(() => {
-    async function fetchProcessData() {
-      // Mantém lógica de fetch caso necessário
-      try {
-        await processService.getProcessByObjectId('resolve_crm', 'project', projectId);
-      } catch (error) {
-        if (error.response?.data?.detail === "No Process matches the given query.") {
-          enqueueSnackbar('Projeto sem processo definido', { variant: 'warning' });
-        } else {
-          console.error('Erro ao buscar processo:', error);
-          enqueueSnackbar(`Erro ao buscar o processo: ${error.response?.data?.detail || error}`, { variant: 'error' });
-        }
-      }
-    }
-    if (projectId) fetchProcessData();
-  }, [projectId, enqueueSnackbar]);
 
   return (
     <Drawer anchor={anchor} open={open} onClose={onClose}>
